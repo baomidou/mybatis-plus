dd6fb7f43903376d2cfd5d2a962bb6aa6927e618
jobob <jobob@qq.com>
2016-09-09 16:58:43 +0800

    新增 typeHanler 级联查询支持

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/mysql/entity/Role.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/mysql/entity/Role.java
@@ -1,11 +1,25 @@
+/**
+ * Copyright (c) 2011-2014, hubin (jobob@qq.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.baomidou.mybatisplus.test.mysql.entity;
+
+import java.io.Serializable;
 
 import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
-
-import java.io.Serializable;
-import java.util.List;
 
 /**
  * <p>
@@ -15,7 +29,7 @@ import java.util.List;
  * @author sjy
  * @Date 2016-09-09
  */
-@TableName(value = "role", resultMap = "RoleMap")
+@TableName(resultMap = "RoleMap")
 public class Role implements Serializable {
 
 	@TableField(exist = false)
@@ -66,14 +80,9 @@ public class Role implements Serializable {
 		this.description = description;
 	}
 
-
 	@Override
 	public String toString() {
-		return "Role{" +
-				"id=" + id +
-				", name='" + name + '\'' +
-				", sort=" + sort +
-				", description='" + description + '\'' +
-				'}';
+		return "Role{" + "id=" + id + ", name='" + name + '\'' + ", sort=" + sort 
+				+ ", description='" + description + '\'' + '}';
 	}
 }
