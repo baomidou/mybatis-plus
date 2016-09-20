5bae0443176850ce32ef2ae1926155b4db8b88d4
jobob <jobob@qq.com>
2016-09-20 11:22:03 +0800

    测试修改

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/framework/RestResponse.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/framework/RestResponse.java
@@ -35,4 +35,5 @@ public class RestResponse<T> {
 	public void setData(T data) {
 		this.data = data;
 	}
+
 }
--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/framework/UserController.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/framework/UserController.java
@@ -2,9 +2,13 @@ package com.baomidou.mybatisplus.test.framework;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.baomidou.mybatisplus.test.framework.service.IUserService;
+import com.baomidou.mybatisplus.test.mysql.entity.User;
 
 @Controller
 @RequestMapping("/user")
@@ -13,31 +17,48 @@ public class UserController {
 	@Autowired
 	private IUserService userService;
 
-//	@ResponseBody
-//	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
-//	public RestResponse<User> getUser(@PathVariable("id") int id) {
-//		User user = userService.getUser(id);
-//		return new RestResponse<User>(true, "", user);
-//	}
-//
-//	@ResponseBody
-//	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
-//	public RestResponse saveUser(@RequestBody User user) {
-//		userService.saveUser(user);
-//		return new RestResponse<String>(true, "", null);
-//	}
-//
-//	@ResponseBody
-//	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
-//	public RestResponse deleteUser(@PathVariable("id") int id) {
-//		userService.deleteUser(id);
-//		return new RestResponse<String>(true, "", null);
-//	}
-//
-//	@ResponseBody
-//	@RequestMapping(method = RequestMethod.PUT, produces = "application/json")
-//	public RestResponse updateUser(@RequestBody User user) {
-//		userService.updateUser(user);
-//		return new RestResponse<String>(true, "", null);
-//	}
+	@ResponseBody
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+	public RestResponse<User> getUser(@PathVariable("id") Long id) {
+		User user = userService.selectById(id);
+		return retRestResponse(user);
+	}
+
+	//
+	// @ResponseBody
+	// @RequestMapping(method = RequestMethod.POST, produces =
+	// "application/json")
+	// public RestResponse saveUser(@RequestBody User user) {
+	// userService.saveUser(user);
+	// return new RestResponse<String>(true, "", null);
+	// }
+	//
+	// @ResponseBody
+	// @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces
+	// = "application/json")
+	// public RestResponse deleteUser(@PathVariable("id") int id) {
+	// userService.deleteUser(id);
+	// return new RestResponse<String>(true, "", null);
+	// }
+	//
+	// @ResponseBody
+	// @RequestMapping(method = RequestMethod.PUT, produces =
+	// "application/json")
+	// public RestResponse updateUser(@RequestBody User user) {
+	// userService.updateUser(user);
+	// return new RestResponse<String>(true, "", null);
+	// }
+
+	public RestResponse<User> retRestResponse() {
+		return retRestResponse(null);
+	}
+
+	public RestResponse<User> retRestResponse(User user) {
+		return retRestResponse("", user);
+	}
+
+	public RestResponse<User> retRestResponse(String message, User user) {
+		return new RestResponse<User>(true, message, user);
+	}
+
 }
