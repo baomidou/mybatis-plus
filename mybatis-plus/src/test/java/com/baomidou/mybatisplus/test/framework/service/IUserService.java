c9c68f0c9eefcd4fdfd81007fb72e52eaefb79c9
jobob <jobob@qq.com>
2016-09-21 11:56:40 +0800

    去掉 framework 部分测试代码

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/framework/service/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.baomidou.mybatisplus.test.framework.service;
-
-import com.baomidou.framework.service.ISuperService;
-import com.baomidou.mybatisplus.test.mysql.entity.User;
-
-public interface IUserService extends ISuperService<User> {
-	
-}
