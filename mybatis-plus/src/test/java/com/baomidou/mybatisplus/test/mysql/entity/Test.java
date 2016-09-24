b381812560d017da7537c5ce863613f5d8d4b092
Caratacus <330952486@qq.com>
2016-09-25 03:59:38 +0800

    添加无XML注入SQL测试类

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/mysql/entity/Test.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/mysql/entity/Test.java
@@ -42,4 +42,20 @@ public class Test implements Serializable {
 	private Long id;
 
 	private String type;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 }
