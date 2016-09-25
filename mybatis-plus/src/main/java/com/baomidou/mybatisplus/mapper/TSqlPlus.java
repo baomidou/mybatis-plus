c13988400c5d46d5de1569d982d904804d6db86e
Caratacus <330952486@qq.com>
2016-09-25 13:22:15 +0800

    完善EntityWrapper(感谢D.Yang提出的BETWEEN AND)

--- mybatis-plus/src/main/java/com/baomidou/mybatisplus/mapper/TSqlPlus.java
+++ mybatis-plus/src/main/java/com/baomidou/mybatisplus/mapper/TSqlPlus.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2011-2020, hubin (jobob@qq.com).
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -33,242 +33,249 @@ import java.util.List;
 @SuppressWarnings("serial")
 public class TSqlPlus extends MybatisAbstractSQL<TSqlPlus> {
 
-	private final String IS_NOT_NULL = " IS NOT NULL";
-	private final String IS_NULL = " IS NULL";
-	private final String SQL_LIKE = " LIKE CONCAT(CONCAT({0},{1}),{2})";
+    private final String IS_NOT_NULL = " IS NOT NULL";
+    private final String IS_NULL = " IS NULL";
+    private final String SQL_LIKE = " LIKE CONCAT(CONCAT({0},{1}),{2})";
+    private final String SQL_BETWEEN_AND = " BETWEEN {0} AND {1}";
 
-	@Override
-	public TSqlPlus getSelf() {
-		return this;
-	}
+    @Override
+    public TSqlPlus getSelf() {
+        return this;
+    }
 
-	/**
-	 * 将LIKE语句添加到WHERE条件中
-	 *
-	 * @param column
-	 *            字段名
-	 * @param value
-	 *            like值,无需前后%, MYSQL及ORACEL通用
-	 * @return
-	 */
-	public TSqlPlus LIKE(String column, String value) {
-		handerLike(column, value, false);
-		return this;
-	}
+    /**
+     * 将LIKE语句添加到WHERE条件中
+     *
+     * @param column 字段名
+     * @param value  like值,无需前后%, MYSQL及ORACEL通用
+     * @return
+     */
+    public TSqlPlus LIKE(String column, String value) {
+        handerLike(column, value, false);
+        return this;
+    }
 
-	/**
-	 * 将LIKE语句添加到WHERE条件中
-	 *
-	 * @param column
-	 *            字段名
-	 * @param value
-	 *            like值,无需前后%, MYSQL及ORACEL通用
-	 * @return
-	 */
-	public TSqlPlus NOT_LIKE(String column, String value) {
-		handerLike(column, value, true);
-		return this;
-	}
+    /**
+     * 将LIKE语句添加到WHERE条件中
+     *
+     * @param column 字段名
+     * @param value  like值,无需前后%, MYSQL及ORACEL通用
+     * @return
+     */
+    public TSqlPlus NOT_LIKE(String column, String value) {
+        handerLike(column, value, true);
+        return this;
+    }
 
-	/**
-	 * IS NOT NULL查询
-	 *
-	 * @param columns
-	 *            以逗号分隔的字段名称
-	 * @return this
-	 */
-	public TSqlPlus IS_NOT_NULL(String columns) {
-		handerNull(columns, IS_NOT_NULL);
-		return this;
-	}
+    /**
+     * IS NOT NULL查询
+     *
+     * @param columns 以逗号分隔的字段名称
+     * @return this
+     */
+    public TSqlPlus IS_NOT_NULL(String columns) {
+        handerNull(columns, IS_NOT_NULL);
+        return this;
+    }
 
-	/**
-	 * IS NULL查询
-	 *
-	 * @param columns
-	 *            以逗号分隔的字段名称
-	 * @return
-	 */
-	public TSqlPlus IS_NULL(String columns) {
-		handerNull(columns, IS_NULL);
-		return this;
-	}
+    /**
+     * IS NULL查询
+     *
+     * @param columns 以逗号分隔的字段名称
+     * @return
+     */
+    public TSqlPlus IS_NULL(String columns) {
+        handerNull(columns, IS_NULL);
+        return this;
+    }
 
-	/**
-	 * 处理LIKE操作
-	 *
-	 * @param column
-	 *            字段名称
-	 * @param value
-	 *            like匹配值
-	 * @param isNot
-	 *            是否为NOT LIKE操作
-	 */
-	private void handerLike(String column, String value, boolean isNot) {
-		if (StringUtils.isNotEmpty(column) && StringUtils.isNotEmpty(value)) {
-			StringBuilder inSql = new StringBuilder();
-			inSql.append(column);
-			if (isNot) {
-				inSql.append(" NOT");
-			}
-			inSql.append(MessageFormat.format(SQL_LIKE, "'%'", StringUtils.quotaMark(value), "'%'"));
-			WHERE(inSql.toString());
-		}
-	}
+    /**
+     * 处理LIKE操作
+     *
+     * @param column 字段名称
+     * @param value  like匹配值
+     * @param isNot  是否为NOT LIKE操作
+     */
+    private void handerLike(String column, String value, boolean isNot) {
+        if (StringUtils.isNotEmpty(column) && StringUtils.isNotEmpty(value)) {
+            StringBuilder inSql = new StringBuilder();
+            inSql.append(column);
+            if (isNot) {
+                inSql.append(" NOT");
+            }
+            inSql.append(MessageFormat.format(SQL_LIKE, "'%'", StringUtils.quotaMark(value), "'%'"));
+            WHERE(inSql.toString());
+        }
+    }
 
-	/**
-	 * 将IN语句添加到WHERE条件中
-	 *
-	 * @param column 字段名
-	 * @param value  List集合
-	 * @return
-	 */
-	public TSqlPlus IN(String column, List<?> value) {
-		handerIn(column, value, false);
-		return this;
-	}
+    /**
+     * 将IN语句添加到WHERE条件中
+     *
+     * @param column 字段名
+     * @param value  List集合
+     * @return
+     */
+    public TSqlPlus IN(String column, List<?> value) {
+        handerIn(column, value, false);
+        return this;
+    }
 
-	/**
-	 * 将IN语句添加到WHERE条件中
-	 *
-	 * @param column 字段名
-	 * @param value  List集合
-	 * @return
-	 */
-	public TSqlPlus NOT_IN(String column, List<?> value) {
-		handerIn(column, value, true);
-		return this;
-	}
+    /**
+     * 将IN语句添加到WHERE条件中
+     *
+     * @param column 字段名
+     * @param value  List集合
+     * @return
+     */
+    public TSqlPlus NOT_IN(String column, List<?> value) {
+        handerIn(column, value, true);
+        return this;
+    }
 
-	/**
-	 * 将IN语句添加到WHERE条件中
-	 *
-	 * @param column 字段名
-	 * @param value  逗号拼接的字符串
-	 * @return
-	 */
-	public TSqlPlus IN(String column, String value) {
-		handerIn(column, value, false);
-		return this;
-	}
+    /**
+     * 将IN语句添加到WHERE条件中
+     *
+     * @param column 字段名
+     * @param value  逗号拼接的字符串
+     * @return
+     */
+    public TSqlPlus IN(String column, String value) {
+        handerIn(column, value, false);
+        return this;
+    }
 
-	/**
-	 * 将IN语句添加到WHERE条件中
-	 *
-	 * @param column 字段名
-	 * @param value  逗号拼接的字符串
-	 * @return
-	 */
-	public TSqlPlus NOT_IN(String column, String value) {
-		handerIn(column, value, true);
-		return this;
-	}
+    /**
+     * 将IN语句添加到WHERE条件中
+     *
+     * @param column 字段名
+     * @param value  逗号拼接的字符串
+     * @return
+     */
+    public TSqlPlus NOT_IN(String column, String value) {
+        handerIn(column, value, true);
+        return this;
+    }
 
-	/**
-	 * 将EXISTS语句添加到WHERE条件中
-	 *
-	 * @param value
-	 * @return
-	 */
-	public TSqlPlus EXISTS(String value) {
-		handerExists(value, false);
-		return this;
-	}
+    /**
+     * 将EXISTS语句添加到WHERE条件中
+     *
+     * @param value
+     * @return
+     */
+    public TSqlPlus EXISTS(String value) {
+        handerExists(value, false);
+        return this;
+    }
 
-	/**
-	 * 处理EXISTS操作
-	 *
-	 * @param value
-	 * @param isNot 是否为NOT EXISTS操作
-	 */
-	private void handerExists(String value, boolean isNot) {
-		if (StringUtils.isNotEmpty(value)) {
-			StringBuilder inSql = new StringBuilder();
-			if (isNot) {
-				inSql.append(" NOT");
-			}
-			inSql.append(" EXISTS (").append(value).append(")");
-			WHERE(inSql.toString());
-		}
-	}
+    /**
+     * 处理EXISTS操作
+     *
+     * @param value
+     * @param isNot 是否为NOT EXISTS操作
+     */
+    private void handerExists(String value, boolean isNot) {
+        if (StringUtils.isNotEmpty(value)) {
+            StringBuilder inSql = new StringBuilder();
+            if (isNot) {
+                inSql.append(" NOT");
+            }
+            inSql.append(" EXISTS (").append(value).append(")");
+            WHERE(inSql.toString());
+        }
+    }
 
-	/**
-	 * 将NOT_EXISTS语句添加到WHERE条件中
-	 *
-	 * @param value
-	 * @return
-	 */
-	public TSqlPlus NOT_EXISTS(String value) {
-		handerExists(value, true);
-		return this;
-	}
+    /**
+     * 将NOT_EXISTS语句添加到WHERE条件中
+     *
+     * @param value
+     * @return
+     */
+    public TSqlPlus NOT_EXISTS(String value) {
+        handerExists(value, true);
+        return this;
+    }
 
-	/**
-	 * 处理IN操作
-	 *
-	 * @param column 字段名称
-	 * @param value  集合List
-	 * @param isNot  是否为NOT IN操作
-	 */
-	private void handerIn(String column, List<?> value, boolean isNot) {
-		if (StringUtils.isNotEmpty(column) && CollectionUtil.isNotEmpty(value)) {
-			StringBuilder inSql = new StringBuilder();
-			inSql.append(column);
-			if (isNot) {
-				inSql.append(" NOT");
-			}
-			inSql.append(" IN (");
-			int _size = value.size();
-			for (int i = 0; i < _size; i++) {
-				String tempVal = StringUtils.quotaMark(value.get(i));
-				if (i + 1 == _size) {
-					inSql.append(tempVal);
-				} else {
-					inSql.append(tempVal);
-					inSql.append(",");
-				}
-			}
-			inSql.append(")");
-			WHERE(inSql.toString());
-		}
-	}
+    /**
+     * 处理IN操作
+     *
+     * @param column 字段名称
+     * @param value  集合List
+     * @param isNot  是否为NOT IN操作
+     */
+    private void handerIn(String column, List<?> value, boolean isNot) {
+        if (StringUtils.isNotEmpty(column) && CollectionUtil.isNotEmpty(value)) {
+            StringBuilder inSql = new StringBuilder();
+            inSql.append(column);
+            if (isNot) {
+                inSql.append(" NOT");
+            }
+            inSql.append(" IN (");
+            int _size = value.size();
+            for (int i = 0; i < _size; i++) {
+                String tempVal = StringUtils.quotaMark(value.get(i));
+                if (i + 1 == _size) {
+                    inSql.append(tempVal);
+                } else {
+                    inSql.append(tempVal);
+                    inSql.append(",");
+                }
+            }
+            inSql.append(")");
+            WHERE(inSql.toString());
+        }
+    }
 
-	/**
-	 * 处理IN操作
-	 *
-	 * @param column 字段名称
-	 * @param value  逗号拼接的字符串
-	 * @param isNot  是否为NOT IN操作
-	 */
-	private void handerIn(String column, String value, boolean isNot) {
-		if (StringUtils.isNotEmpty(column) && StringUtils.isNotEmpty(value)) {
-			StringBuilder inSql = new StringBuilder();
-			inSql.append(column);
-			if (isNot) {
-				inSql.append(" NOT");
-			}
-			inSql.append(" IN (").append(value).append(")");
-			WHERE(inSql.toString());
-		}
-	}
+    /**
+     * 处理IN操作
+     *
+     * @param column 字段名称
+     * @param value  逗号拼接的字符串
+     * @param isNot  是否为NOT IN操作
+     */
+    private void handerIn(String column, String value, boolean isNot) {
+        if (StringUtils.isNotEmpty(column) && StringUtils.isNotEmpty(value)) {
+            StringBuilder inSql = new StringBuilder();
+            inSql.append(column);
+            if (isNot) {
+                inSql.append(" NOT");
+            }
+            inSql.append(" IN (").append(value).append(")");
+            WHERE(inSql.toString());
+        }
+    }
 
-	/**
-	 * 以相同的方式处理null和notnull
-	 *
-	 * @param columns
-	 *            以逗号分隔的字段名称
-	 * @param sqlPart
-	 *            SQL部分
-	 */
-	private void handerNull(String columns, String sqlPart) {
-		if (StringUtils.isNotEmpty(columns)) {
-			String[] cols = columns.split(",");
-			for (String col : cols) {
-				if (StringUtils.isNotEmpty(col.trim())) {
-					WHERE(col + sqlPart);
-				}
-			}
-		}
-	}
+    /**
+     * 处理BETWEEN_AND操作
+     *
+     * @param column 字段名称
+     * @param val1
+     * @param val2
+     */
+    public TSqlPlus BETWEEN_AND(String column, String val1, String val2) {
+        if (StringUtils.isNotEmpty(column) && StringUtils.isNotEmpty(val1) && StringUtils.isNotEmpty(val2)) {
+            StringBuilder betweenSql = new StringBuilder();
+            betweenSql.append(column);
+            betweenSql.append(MessageFormat.format(SQL_BETWEEN_AND, val1, val2));
+            WHERE(betweenSql.toString());
+        }
+        return this;
+    }
+
+    /**
+     * 以相同的方式处理null和notnull
+     *
+     * @param columns 以逗号分隔的字段名称
+     * @param sqlPart SQL部分
+     */
+    private void handerNull(String columns, String sqlPart) {
+        if (StringUtils.isNotEmpty(columns)) {
+            String[] cols = columns.split(",");
+            for (String col : cols) {
+                if (StringUtils.isNotEmpty(col.trim())) {
+                    WHERE(col + sqlPart);
+                }
+            }
+        }
+    }
 
 }
--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/EntityWrapperTest.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/EntityWrapperTest.java
@@ -34,203 +34,213 @@ import java.util.List;
  */
 public class EntityWrapperTest {
 
-    /*
-     * User 查询包装器
-     */
-    private EntityWrapper<User> ew = new EntityWrapper<User>();
+	/*
+	 * User 查询包装器
+	 */
+	private EntityWrapper<User> ew = new EntityWrapper<User>();
 
-    // 初始化
-    static {
-        TableInfoHelper.initTableInfo(User.class);
-    }
+	// 初始化
+	static {
+		TableInfoHelper.initTableInfo(User.class);
+	}
 
-    @Test
-    public void test() {
-        /*
-         * 无条件测试
+	@Test
+	public void test() {
+		/*
+		 * 无条件测试
 		 */
-        Assert.assertNull(ew.getSqlSegment());
-    }
+		Assert.assertNull(ew.getSqlSegment());
+	}
 
-    @Test
-    public void test11() {
-        /*
-         * 实体带where   ifneed
-         */
-        ew.setEntity(new User(1));
-        ew.where("name={0}", "'123'").addFilterIfNeed(false, "id=12");
-        String sqlSegment = ew.getSqlSegment();
-        System.err.println("test11 = " + sqlSegment);
-        Assert.assertEquals("AND (name='123')", sqlSegment);
-    }
-
-    @Test
-    public void test12() {
-        /*
-         * 实体带where orderby
-         */
-        ew.setEntity(new User(1));
-        ew.where("name={0}", "'123'").orderBy("id", false);
-        String sqlSegment = ew.getSqlSegment();
-        System.err.println("test12 = " + sqlSegment);
-        Assert.assertEquals("AND (name='123')\nORDER BY id DESC", sqlSegment);
-    }
-
-    @Test
-    public void test13() {
-        /*
-         * 实体排序
+	@Test
+	public void test11() {
+		/*
+		 * 实体带where ifneed
 		 */
-        ew.setEntity(new User(1));
-        ew.orderBy("id", false);
-        String sqlSegment = ew.getSqlSegment();
-        System.err.println("test13 = " + sqlSegment);
-        Assert.assertEquals("ORDER BY id DESC", sqlSegment);
-    }
+		ew.setEntity(new User(1));
+		ew.where("name={0}", "'123'").addFilterIfNeed(false, "id=12");
+		String sqlSegment = ew.getSqlSegment();
+		System.err.println("test11 = " + sqlSegment);
+		Assert.assertEquals("AND (name='123')", sqlSegment);
+	}
 
-    @Test
-    public void test21() {
-        /*
-         * 无实体 where ifneed orderby
-         */
-        ew.where("name={0}", "'123'").addFilterIfNeed(false, "id=1").orderBy("id");
-        String sqlSegment = ew.getSqlSegment();
-        System.err.println("test21 = " + sqlSegment);
-        Assert.assertEquals("WHERE (name='123')\nORDER BY id", sqlSegment);
-    }
-
-    @Test
-    public void test22() {
-        ew.where("name={0}", "'123'").orderBy("id", false);
-        String sqlSegment = ew.getSqlSegment();
-        System.err.println("test22 = " + sqlSegment);
-        Assert.assertEquals("WHERE (name='123')\nORDER BY id DESC", sqlSegment);
-    }
-
-    @Test
-    public void test23() {
-        /*
-         * 无实体查询，只排序
+	@Test
+	public void test12() {
+		/*
+		 * 实体带where orderby
 		 */
-        ew.orderBy("id", false);
-        String sqlSegment = ew.getSqlSegment();
-        System.err.println("test23 = " + sqlSegment);
-        Assert.assertEquals("ORDER BY id DESC", sqlSegment);
-    }
+		ew.setEntity(new User(1));
+		ew.where("name={0}", "'123'").orderBy("id", false);
+		String sqlSegment = ew.getSqlSegment();
+		System.err.println("test12 = " + sqlSegment);
+		Assert.assertEquals("AND (name='123')\nORDER BY id DESC", sqlSegment);
+	}
 
-    @Test
-    public void testNoTSQL() {
-        /*
-         * 实体 filter orderby
+	@Test
+	public void test13() {
+		/*
+		 * 实体排序
 		 */
-        ew.setEntity(new User(1));
-        ew.addFilter("name={0}", "'123'").orderBy("id,name");
-        String sqlSegment = ew.getSqlSegment();
-        System.err.println("testNoTSQL = " + sqlSegment);
-        Assert.assertEquals("AND (name='123')\nORDER BY id,name", sqlSegment);
-    }
+		ew.setEntity(new User(1));
+		ew.orderBy("id", false);
+		String sqlSegment = ew.getSqlSegment();
+		System.err.println("test13 = " + sqlSegment);
+		Assert.assertEquals("ORDER BY id DESC", sqlSegment);
+	}
 
-    @Test
-    public void testNoTSQL1() {
-        /*
-         * 非 T-SQL 无实体查询
+	@Test
+	public void test21() {
+		/*
+		 * 无实体 where ifneed orderby
 		 */
-        ew.addFilter("name={0}", "'123'").addFilterIfNeed(false, "status={1}", "1");
-        String sqlSegment = ew.getSqlSegment();
-        System.err.println("testNoTSQL1 = " + sqlSegment);
-        Assert.assertEquals("WHERE (name='123')", sqlSegment);
-    }
+		ew.where("name={0}", "'123'").addFilterIfNeed(false, "id=1").orderBy("id");
+		String sqlSegment = ew.getSqlSegment();
+		System.err.println("test21 = " + sqlSegment);
+		Assert.assertEquals("WHERE (name='123')\nORDER BY id", sqlSegment);
+	}
 
-    @Test
-    public void testTSQL11() {
-        /*
-         * 实体带查询使用方法  输出看结果
-         */
-        ew.setEntity(new User(1));
-        ew.where("name={0}", "'zhangsan'").and("id=1")
-                .orNew("status={0}", "0").or("status=1")
-                .notLike("nlike", "notvalue")
-                .andNew("new=xx").like("hhh", "ddd")
-                .andNew("pwd=11").isNotNull("n1,n2").isNull("n3")
-                .groupBy("x1").groupBy("x2,x3")
-                .having("x1=11").having("x3=433")
-                .orderBy("dd").orderBy("d1,d2");
-        System.out.println(ew.getSqlSegment());
-    }
+	@Test
+	public void test22() {
+		ew.where("name={0}", "'123'").orderBy("id", false);
+		String sqlSegment = ew.getSqlSegment();
+		System.err.println("test22 = " + sqlSegment);
+		Assert.assertEquals("WHERE (name='123')\nORDER BY id DESC", sqlSegment);
+	}
 
-    @Test
-    public void testNull() {
-        ew.orderBy(null);
-        String sqlPart = ew.getSqlSegment();
-        Assert.assertNull(sqlPart);
-    }
+	@Test
+	public void test23() {
+		/*
+		 * 无实体查询，只排序
+		 */
+		ew.orderBy("id", false);
+		String sqlSegment = ew.getSqlSegment();
+		System.err.println("test23 = " + sqlSegment);
+		Assert.assertEquals("ORDER BY id DESC", sqlSegment);
+	}
 
-    @Test
-    public void testNull2() {
-        ew.like(null, null).where("aa={0}", "'bb'").orderBy(null);
-        String sqlPart = ew.getSqlSegment();
-        Assert.assertEquals("WHERE (aa='bb')", sqlPart);
-    }
+	@Test
+	public void testNoTSQL() {
+		/*
+		 * 实体 filter orderby
+		 */
+		ew.setEntity(new User(1));
+		ew.addFilter("name={0}", "'123'").orderBy("id,name");
+		String sqlSegment = ew.getSqlSegment();
+		System.err.println("testNoTSQL = " + sqlSegment);
+		Assert.assertEquals("AND (name='123')\nORDER BY id,name", sqlSegment);
+	}
 
-    /**
-     * 测试带单引号的值是否不会再次添加单引号
-     */
-    @Test
-    public void testNul14() {
-        ew.where("id={0}", "'11'").and("name={0}", 22);
-        String sqlPart = ew.getSqlSegment();
-        System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (id='11' AND name=22)", sqlPart);
-    }
+	@Test
+	public void testNoTSQL1() {
+		/*
+		 * 非 T-SQL 无实体查询
+		 */
+		ew.addFilter("name={0}", "'123'").addFilterIfNeed(false, "status={1}", "1");
+		String sqlSegment = ew.getSqlSegment();
+		System.err.println("testNoTSQL1 = " + sqlSegment);
+		Assert.assertEquals("WHERE (name='123')", sqlSegment);
+	}
 
-    /**
-     * 测试带不带单引号的值是否会自动添加单引号
-     */
-    @Test
-    public void testNul15() {
-        ew.where("id={0}", "11").and("name={0}", 222222222);
-        String sqlPart = ew.getSqlSegment();
-        System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (id='11' AND name=222222222)", sqlPart);
-    }
+	@Test
+	public void testTSQL11() {
+		/*
+		 * 实体带查询使用方法 输出看结果
+		 */
+		ew.setEntity(new User(1));
+		ew.where("name={0}", "'zhangsan'").and("id=1").orNew("status={0}", "0").or("status=1").notLike("nlike", "notvalue")
+				.andNew("new=xx").like("hhh", "ddd").andNew("pwd=11").isNotNull("n1,n2").isNull("n3").groupBy("x1")
+				.groupBy("x2,x3").having("x1=11").having("x3=433").orderBy("dd").orderBy("d1,d2");
+		System.out.println(ew.getSqlSegment());
+	}
 
-    /**
-     * 测试EXISTS
-     */
-    @Test
-    public void testNul16() {
-        ew.notExists("(select * from user)");
-        String sqlPart = ew.getSqlSegment();
-        System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE ( NOT EXISTS ((select * from user)))", sqlPart);
-    }
-    /**
-     * 测试NOT IN
-     */
-    @Test
-    public void testNul17() {
-        List<String> list = new ArrayList<String>();
-        list.add("'1'");
-        list.add("'2'");
-        list.add("'3'");
-        ew.notIn("test_type",list);
-        String sqlPart = ew.getSqlSegment();
-        System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (test_type NOT IN ('1','2','3'))", sqlPart);
-    }
-    /**
-     * 测试IN
-     */
-    @Test
-    public void testNul18() {
-        List<Long> list = new ArrayList<Long>();
-        list.add(111111111L);
-        list.add(222222222L);
-        list.add(333333333L);
-        ew.in("test_type",list);
-        String sqlPart = ew.getSqlSegment();
-        System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (test_type IN (111111111,222222222,333333333))", sqlPart);
-    }
+	@Test
+	public void testNull() {
+		ew.orderBy(null);
+		String sqlPart = ew.getSqlSegment();
+		Assert.assertNull(sqlPart);
+	}
+
+	@Test
+	public void testNull2() {
+		ew.like(null, null).where("aa={0}", "'bb'").orderBy(null);
+		String sqlPart = ew.getSqlSegment();
+		Assert.assertEquals("WHERE (aa='bb')", sqlPart);
+	}
+
+	/**
+	 * 测试带单引号的值是否不会再次添加单引号
+	 */
+	@Test
+	public void testNul14() {
+		ew.where("id={0}", "'11'").and("name={0}", 22);
+		String sqlPart = ew.getSqlSegment();
+		System.out.println("sql ==> " + sqlPart);
+		Assert.assertEquals("WHERE (id='11' AND name=22)", sqlPart);
+	}
+
+	/**
+	 * 测试带不带单引号的值是否会自动添加单引号
+	 */
+	@Test
+	public void testNul15() {
+		ew.where("id={0}", "11").and("name={0}", 222222222);
+		String sqlPart = ew.getSqlSegment();
+		System.out.println("sql ==> " + sqlPart);
+		Assert.assertEquals("WHERE (id='11' AND name=222222222)", sqlPart);
+	}
+
+	/**
+	 * 测试EXISTS
+	 */
+	@Test
+	public void testNul16() {
+		ew.notExists("(select * from user)");
+		String sqlPart = ew.getSqlSegment();
+		System.out.println("sql ==> " + sqlPart);
+		Assert.assertEquals("WHERE ( NOT EXISTS ((select * from user)))", sqlPart);
+	}
+
+	/**
+	 * 测试NOT IN
+	 */
+	@Test
+	public void testNul17() {
+		List<String> list = new ArrayList<String>();
+		list.add("'1'");
+		list.add("'2'");
+		list.add("'3'");
+		ew.notIn("test_type", list);
+		String sqlPart = ew.getSqlSegment();
+		System.out.println("sql ==> " + sqlPart);
+		Assert.assertEquals("WHERE (test_type NOT IN ('1','2','3'))", sqlPart);
+	}
+
+	/**
+	 * 测试IN
+	 */
+	@Test
+	public void testNul18() {
+		List<Long> list = new ArrayList<Long>();
+		list.add(111111111L);
+		list.add(222222222L);
+		list.add(333333333L);
+		ew.in("test_type", list);
+		String sqlPart = ew.getSqlSegment();
+		System.out.println("sql ==> " + sqlPart);
+		Assert.assertEquals("WHERE (test_type BETWEEN 11 AND 33)", sqlPart);
+	}
+
+	/**
+	 * 测试BETWEEN AND
+	 */
+	@Test
+	public void testNul19() {
+		String val1 = "11";
+		String val2 = "33";
+		ew.between("test_type", val1, val2);
+		String sqlPart = ew.getSqlSegment();
+		System.out.println("sql ==> " + sqlPart);
+		Assert.assertEquals("WHERE (test_type BETWEEN 11 AND 33)", sqlPart);
+	}
 
 }
