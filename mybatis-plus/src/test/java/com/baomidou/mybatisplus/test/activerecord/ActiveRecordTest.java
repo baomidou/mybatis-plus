e45cf6d75d7986327a0e2f6be74a4670b4f51a57
jobob <jobob@qq.com>
2016-10-12 09:40:03 +0800

    简单修改

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/activerecord/ActiveRecordTest.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/activerecord/ActiveRecordTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2011-2020, hubin (jobob@qq.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.baomidou.mybatisplus.test.activerecord;
 
 import com.baomidou.mybatisplus.activerecord.DB;
@@ -8,13 +23,22 @@ import com.baomidou.mybatisplus.toolkit.TableInfoHelper;
 import java.util.List;
 
 /**
- * Created by Caratacus on 2016/10/11 0011.
+ * <p>
+ * ActiveRecord 测试
+ * </p>
+ * 
+ * @author Caratacus
+ * @date 2016-10-11
  */
 public class ActiveRecordTest {
-    public static void main(String[] args) {
-        TableInfoHelper.initTableInfo(Test.class);
-        DB db = DB.open("jdbc:mysql://localhost/mybatis-plus", "root", "521");
-        List<Record> user = db.active("test1").select().all();
-        System.out.println(user);
-    }
+
+	public static void main(String[] args) {
+		TableInfoHelper.initTableInfo(Test.class);
+		DB db = DB.open("jdbc:mysql://localhost/mybatis-plus", "root", "123456");
+		List<Record> test = db.active("test1").select().all();
+		System.out.println(test);
+
+		/* 未找到异常 */
+		db.active("test1").select().all();
+	}
 }
