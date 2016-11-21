941ba9c86b8111aeaf1a06573d67c4f68d87abbe
Caratacus <330952486@qq.com>
2016-11-21 11:26:49 +0800

    注入ByMap方法添加校验(根据Mybatis-Plus全局校验机制校验)

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/mysql/NoXMLTest.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/mysql/NoXMLTest.java
@@ -16,10 +16,10 @@
 package com.baomidou.mybatisplus.test.mysql;
 
 import com.baomidou.mybatisplus.MybatisSessionFactoryBuilder;
+import com.baomidou.mybatisplus.test.mysql.entity.Test;
 import com.baomidou.mybatisplus.toolkit.IdWorker;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.junit.Test;
 
 import java.io.InputStream;
 import java.util.HashMap;
@@ -30,47 +30,31 @@ import java.util.Map;
  * <p>
  * 测试没有XML同样注入CRUD SQL
  * </p>
- * 
+ *
  * @author Caratacus
  * @date 2016-09-26
  */
 public class NoXMLTest {
-	private SqlSession sqlSession;
-	{
+
+	public static void main(String[] args) {
+		/*
+		 * 加载配置文件
+		 */
 		InputStream in = NoXMLTest.class.getClassLoader().getResourceAsStream("mysql-config.xml");
 		MybatisSessionFactoryBuilder mf = new MybatisSessionFactoryBuilder();
 		SqlSessionFactory sessionFactory = mf.build(in);
-		sqlSession = sessionFactory.openSession();
-	}
-
-	/**
-	 * 测试ByMap忽略null
-	 */
-	@Test
-	public void test1() {
+		SqlSession sqlSession = sessionFactory.openSession();
 		/**
 		 * 查询是否有结果
 		 */
 		TestMapper testMapper = sqlSession.getMapper(TestMapper.class);
+		testMapper.insert(new Test(IdWorker.getId(), "Caratacus"));
 		Map<String, Object> map = new HashMap();
 		map.put("type", null);
 		map.put("id", null);
-		System.out.println(testMapper.selectByMap(map));
-	}
-
-	/**
-	 * 测试没有XML注入CRUD
-	 */
-	@Test
-	public void test2() {
-		/**
-		 * 查询是否有结果
-		 */
-		TestMapper testMapper = sqlSession.getMapper(TestMapper.class);
-		testMapper.insert(new com.baomidou.mybatisplus.test.mysql.entity.Test(IdWorker.getId(), "Caratacus"));
-		List<com.baomidou.mybatisplus.test.mysql.entity.Test> tests = testMapper.selectList(null);
+		List<Test> tests = testMapper.selectByMap(map);
 		if (null != tests) {
-			for (com.baomidou.mybatisplus.test.mysql.entity.Test test : tests) {
+			for (Test test : tests) {
 				System.out.println("id:"+test.getId()+ " , type:" + test.getType());
 			}
 		} else {
