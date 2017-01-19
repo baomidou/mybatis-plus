8d4d441c740ff8b4626f0893170b4b17f21658f6
jobob <jobob@qq.com>
2017-01-19 14:16:03 +0800

    添加注释

--- mybatis-plus/src/main/java/com/baomidou/mybatisplus/annotations/TableField.java
+++ mybatis-plus/src/main/java/com/baomidou/mybatisplus/annotations/TableField.java
@@ -34,21 +34,25 @@ import com.baomidou.mybatisplus.enums.FieldStrategy;
 @Target(ElementType.FIELD)
 public @interface TableField {
 
-	/*
+	/**
 	 * <p>
 	 * 字段值（驼峰命名方式，该值可无）
 	 * </p>
 	 */
 	String value() default "";
 
-	/*
+	/**
 	 * <p>
 	 * 当该Field为类对象时, 可使用#{对象.属性}来映射到数据表.
+	 * </p>
+	 * <p>
+	 * 支持：@TableField(el = "role, jdbcType=BIGINT)<br>
+	 * 支持：@TableField(el = "role, typeHandler=com.baomidou.xx.typehandler.PhoneTypeHandler")
 	 * </p>
 	 */
 	String el() default "";
 
-	/*
+	/**
 	 * <p>
 	 * 是否为数据库表字段
 	 * </p>
@@ -57,8 +61,8 @@ public @interface TableField {
 	 * </p>
 	 */
 	boolean exist() default true;
-	
-	/*
+
+	/**
 	 * <p>
 	 * 字段验证
 	 * </p>
