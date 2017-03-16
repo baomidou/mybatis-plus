b80a50a4b9dd78ff2388612fccf6194a5f7f5a9a
bj_renyong <bj_renyong@163.com>
2017-03-16 16:54:12 +0800

    MyBaseMapper需要从com.baomidou.mybatisplus.test.mysql.mapper中移除，因为它不是某具体表的mapper，无法扫描注入CRUD代码

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/mysql/MyBaseMapper.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/mysql/MyBaseMapper.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.baomidou.mybatisplus.test.mysql.mapper;
+package com.baomidou.mybatisplus.test.mysql;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 
