673fc69683a695cb49a3a4ba3c3556fa5b389416
Caratacus <330952486@qq.com>
2017-06-15 21:44:59 +0800

    update Wrapper tests

--- src/test/java/com/baomidou/mybatisplus/test/EntityWrapperTest.java
+++ src/test/java/com/baomidou/mybatisplus/test/EntityWrapperTest.java
@@ -58,7 +58,7 @@ public class EntityWrapperTest {
         /*
          * 无条件测试
 		 */
-        Assert.assertNull(ew.toString());
+        Assert.assertEquals("",ew.originalSql());
     }
 
     @Test
@@ -68,7 +68,7 @@ public class EntityWrapperTest {
 		 */
         ew.setEntity(new User(1));
         ew.where("name={0}", "'123'").addFilterIfNeed(false, "id=12");
-        String sqlSegment = ew.toString();
+        String sqlSegment = ew.originalSql();
         System.err.println("test11 = " + sqlSegment);
         Assert.assertEquals("AND (name=?)", sqlSegment);
     }
@@ -80,7 +80,7 @@ public class EntityWrapperTest {
 		 */
         ew.setEntity(new User(1));
         ew.where("name={0}", "'123'").orderBy("id", false);
-        String sqlSegment = ew.toString();
+        String sqlSegment = ew.originalSql();
         System.err.println("test12 = " + sqlSegment);
         Assert.assertEquals("AND (name=?)\nORDER BY id DESC", sqlSegment);
     }
@@ -92,7 +92,7 @@ public class EntityWrapperTest {
 		 */
         ew.setEntity(new User(1));
         ew.orderBy("id", false);
-        String sqlSegment = ew.toString();
+        String sqlSegment = ew.originalSql();
         System.err.println("test13 = " + sqlSegment);
         Assert.assertEquals("ORDER BY id DESC", sqlSegment);
     }
@@ -103,17 +103,17 @@ public class EntityWrapperTest {
 		 * 无实体 where ifneed orderby
 		 */
         ew.where("name={0}", "'123'").addFilterIfNeed(false, "id=1").orderBy("id");
-        String sqlSegment = ew.toString();
+        String sqlSegment = ew.originalSql();
         System.err.println("test21 = " + sqlSegment);
-        Assert.assertEquals("WHERE (name=?)\nORDER BY id", sqlSegment);
+        Assert.assertEquals("AND (name=?)\nORDER BY id", sqlSegment);
     }
 
     @Test
     public void test22() {
         ew.where("name={0}", "'123'").orderBy("id", false);
-        String sqlSegment = ew.toString();
+        String sqlSegment = ew.originalSql();
         System.err.println("test22 = " + sqlSegment);
-        Assert.assertEquals("WHERE (name=?)\nORDER BY id DESC", sqlSegment);
+        Assert.assertEquals("AND (name=?)\nORDER BY id DESC", sqlSegment);
     }
 
     @Test
@@ -122,7 +122,7 @@ public class EntityWrapperTest {
 		 * 无实体查询，只排序
 		 */
         ew.orderBy("id", false);
-        String sqlSegment = ew.toString();
+        String sqlSegment = ew.originalSql();
         System.err.println("test23 = " + sqlSegment);
         Assert.assertEquals("ORDER BY id DESC", sqlSegment);
     }
@@ -134,7 +134,7 @@ public class EntityWrapperTest {
 		 */
         ew.setEntity(new User(1));
         ew.addFilter("name={0}", "'123'").orderBy("id,name");
-        String sqlSegment = ew.toString();
+        String sqlSegment = ew.originalSql();
         System.err.println("testNoTSQL = " + sqlSegment);
         Assert.assertEquals("AND (name=?)\nORDER BY id,name", sqlSegment);
     }
@@ -145,9 +145,9 @@ public class EntityWrapperTest {
 		 * 非 T-SQL 无实体查询
 		 */
         ew.addFilter("name={0}", "'123'").addFilterIfNeed(false, "status=?", "1");
-        String sqlSegment = ew.toString();
+        String sqlSegment = ew.originalSql();
         System.err.println("testNoTSQL1 = " + sqlSegment);
-        Assert.assertEquals("WHERE (name=?)", sqlSegment);
+        Assert.assertEquals("AND (name=?)", sqlSegment);
     }
 
     @Test
@@ -159,24 +159,24 @@ public class EntityWrapperTest {
         ew.where("name=?", "'zhangsan'").and("id=1").orNew("status=?", "0").or("status=1").notLike("nlike", "notvalue")
                 .andNew("new=xx").like("hhh", "ddd").andNew("pwd=11").isNotNull("n1,n2").isNull("n3").groupBy("x1")
                 .groupBy("x2,x3").having("x1=11").having("x3=433").orderBy("dd").orderBy("d1,d2");
-        System.out.println(ew.toString());
+        System.out.println(ew.originalSql());
         Assert.assertEquals("AND (name=? AND id=1) \n" + "OR (status=? OR status=1 AND nlike NOT LIKE ?) \n"
                 + "AND (new=xx AND hhh LIKE ?) \n" + "AND (pwd=11 AND n1 IS NOT NULL AND n2 IS NOT NULL AND n3 IS NULL)\n"
-                + "GROUP BY x1, x2,x3\n" + "HAVING (x1=11 AND x3=433)\n" + "ORDER BY dd, d1,d2", ew.toString());
+                + "GROUP BY x1, x2,x3\n" + "HAVING (x1=11 AND x3=433)\n" + "ORDER BY dd, d1,d2", ew.originalSql());
     }
 
     @Test
     public void testNull() {
         ew.orderBy(null);
-        String sqlPart = ew.toString();
-        Assert.assertNull(sqlPart);
+        String sqlPart = ew.originalSql();
+        Assert.assertEquals("",sqlPart);
     }
 
     @Test
     public void testNull2() {
         ew.like(null, null).where("aa={0}", "'bb'").orderBy(null);
-        String sqlPart = ew.toString();
-        Assert.assertEquals("WHERE (aa=?)", sqlPart);
+        String sqlPart = ew.originalSql();
+        Assert.assertEquals("AND (aa=?)", sqlPart);
     }
 
     /**
@@ -185,9 +185,9 @@ public class EntityWrapperTest {
     @Test
     public void testNul14() {
         ew.where("id={0}", "'11'").and("name={0}", 22);
-        String sqlPart = ew.toString();
+        String sqlPart = ew.originalSql();
         System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (id=? AND name=?)", sqlPart);
+        Assert.assertEquals("AND (id=? AND name=?)", sqlPart);
     }
 
     /**
@@ -196,9 +196,9 @@ public class EntityWrapperTest {
     @Test
     public void testNul15() {
         ew.where("id={0} and ids = {1}", "11", 22).and("name={0}", 222222222);
-        String sqlPart = ew.toString();
+        String sqlPart = ew.originalSql();
         System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (id=? and ids = ? AND name=?)", sqlPart);
+        Assert.assertEquals("AND (id=? and ids = ? AND name=?)", sqlPart);
     }
 
     /**
@@ -207,9 +207,9 @@ public class EntityWrapperTest {
     @Test
     public void testNul16() {
         ew.notExists("(select * from user)");
-        String sqlPart = ew.toString();
+        String sqlPart = ew.originalSql();
         System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE ( NOT EXISTS ((select * from user)))", sqlPart);
+        Assert.assertEquals("AND ( NOT EXISTS ((select * from user)))", sqlPart);
     }
 
     /**
@@ -222,9 +222,9 @@ public class EntityWrapperTest {
         list.add("'2'");
         list.add("'3'");
         ew.notIn("test_type", list);
-        String sqlPart = ew.toString();
+        String sqlPart = ew.originalSql();
         System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (test_type NOT IN (?,?,?))", sqlPart);
+        Assert.assertEquals("AND (test_type NOT IN (?,?,?))", sqlPart);
     }
 
     /**
@@ -237,9 +237,9 @@ public class EntityWrapperTest {
         list.add(222222222L);
         list.add(333333333L);
         ew.in("test_type", list);
-        String sqlPart = ew.toString();
+        String sqlPart = ew.originalSql();
         System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (test_type IN (?,?,?))", sqlPart);
+        Assert.assertEquals("AND (test_type IN (?,?,?))", sqlPart);
     }
 
     /**
@@ -252,9 +252,9 @@ public class EntityWrapperTest {
         list.add(222222222L);
         list.add(333333333L);
         ew.in("test_type", list);
-        String sqlPart = ew.toString();
+        String sqlPart = ew.originalSql();
         System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (test_type IN (?,?,?))", sqlPart);
+        Assert.assertEquals("AND (test_type IN (?,?,?))", sqlPart);
     }
 
     /**
@@ -265,9 +265,9 @@ public class EntityWrapperTest {
         String val1 = "11";
         String val2 = "33";
         ew.between("test_type", val1, val2);
-        String sqlPart = ew.toString();
+        String sqlPart = ew.originalSql();
         System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (test_type BETWEEN ? AND ?)", sqlPart);
+        Assert.assertEquals("AND (test_type BETWEEN ? AND ?)", sqlPart);
     }
 
     /**
@@ -278,9 +278,9 @@ public class EntityWrapperTest {
         String val1 = "'''";
         String val2 = "\\";
         ew.between("test_type", val1, val2);
-        String sqlPart = ew.toString();
+        String sqlPart = ew.originalSql();
         System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (test_type BETWEEN ? AND ?)", sqlPart);
+        Assert.assertEquals("AND (test_type BETWEEN ? AND ?)", sqlPart);
     }
 
     /**
@@ -290,9 +290,9 @@ public class EntityWrapperTest {
     public void testInstance() {
         String val1 = "'''";
         String val2 = "\\";
-        String sqlPart = Condition.create().between("test_type", val1, val2).toString();
+        String sqlPart = Condition.create().between("test_type", val1, val2).originalSql();
         System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (test_type BETWEEN ? AND ?)", sqlPart);
+        Assert.assertEquals("AND (test_type BETWEEN ? AND ?)", sqlPart);
     }
 
     /**
@@ -305,10 +305,10 @@ public class EntityWrapperTest {
         map.put("allEq1", "22");
         map.put("allEq2", 3333);
         map.put("allEq3", 66.99);
-        String sqlPart = Condition.create().gt("gt", 1).le("le", 2).lt("le", 3).ge("ge", 4).eq("eq", 5).allEq(map).toString();
+        String sqlPart = Condition.create().gt("gt", 1).le("le", 2).lt("le", 3).ge("ge", 4).eq("eq", 5).allEq(map).originalSql();
         System.out.println("sql ==> " + sqlPart);
         Assert.assertEquals(
-                "WHERE (gt > ? AND le <= ? AND le < ? AND ge >= ? AND eq = ? AND allEq3 = ? AND allEq1 = ? AND allEq2 = ?)",
+                "AND (gt > ? AND le <= ? AND le < ? AND ge >= ? AND eq = ? AND allEq3 = ? AND allEq1 = ? AND allEq2 = ?)",
                 sqlPart);
     }
 
@@ -318,9 +318,9 @@ public class EntityWrapperTest {
     @Test
     public void testlike() {
         String sqlPart = Condition.create().like("default", "default", SqlLike.DEFAULT).like("left", "left", SqlLike.LEFT)
-                .like("right", "right", SqlLike.RIGHT).toString();
+                .like("right", "right", SqlLike.RIGHT).originalSql();
         System.out.println("sql ==> " + sqlPart);
-        Assert.assertEquals("WHERE (default LIKE ? AND left LIKE ? AND right LIKE ?)", sqlPart);
+        Assert.assertEquals("AND (default LIKE ? AND left LIKE ? AND right LIKE ?)", sqlPart);
     }
 
     /**
@@ -336,7 +336,7 @@ public class EntityWrapperTest {
         ew.or("sql = {0}", "sql").like("default", "default", SqlLike.DEFAULT).like("left", "left", SqlLike.LEFT);
         ew.in("aaabbbcc", "1,3,4");
         String sqlPart = ew.in("bbb", Arrays.asList(new String[]{"a", "b", "c"})).like("right", "right", SqlLike.RIGHT).isWhere(true)
-                .eq("bool", true).between("ee", "1111", "222").toString();
+                .eq("bool", true).between("ee", "1111", "222").originalSql();
         System.out.println("sql ==> " + sqlPart);
         Assert.assertEquals("WHERE (sql = ? AND default LIKE ? AND left LIKE ? AND aaabbbcc IN (?,?,?) AND bbb IN (?,?,?) AND right LIKE ? AND bool = ? AND ee BETWEEN ? AND ?)", sqlPart);
         System.out.println(ew.getSqlSegment());
@@ -349,9 +349,9 @@ public class EntityWrapperTest {
     public void testLimit() {
         ew.where("name={0}", "'123'").orderBy("id", false);
         ew.last("limit 1,2");
-        String sqlSegment = ew.toString();
+        String sqlSegment = ew.originalSql();
         System.err.println("testLimit = " + sqlSegment);
-        Assert.assertEquals("WHERE (name=?)\nORDER BY id DESC limit 1,2", sqlSegment);
+        Assert.assertEquals("AND (name=?)\nORDER BY id DESC limit 1,2", sqlSegment);
     }
 
     /**
