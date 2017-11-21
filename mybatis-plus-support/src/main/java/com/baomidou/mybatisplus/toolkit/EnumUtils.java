6b5420423b915acc17a598d2b1c25aa33b698bea
= <=>
2017-11-21 23:41:25 +0800

    升级 2.1.6 分包，代号：小秋秋之吻

--- mybatis-plus-support/src/main/java/com/baomidou/mybatisplus/toolkit/EnumUtils.java
+++ mybatis-plus-support/src/main/java/com/baomidou/mybatisplus/toolkit/EnumUtils.java
@@ -18,6 +18,8 @@ package com.baomidou.mybatisplus.toolkit;
 
 import com.baomidou.mybatisplus.enums.IEnum;
 
+import java.math.BigDecimal;
+
 /**
  * <p>
  * 枚举处理工具类
@@ -41,8 +43,17 @@ public class EnumUtils {
     public static <E extends Enum<?> & IEnum> E valueOf(Class<E> enumClass, Object value) {
         E[] es = enumClass.getEnumConstants();
         for (E e : es) {
-            if ((value instanceof String && e.getValue().equals(value))
-                    || e.getValue() == value) {
+            if (e.getValue() == value) {
+                // 基本类型
+                return e;
+            } else if (value instanceof String && e.getValue().equals(value)) {
+                // 字符串类型
+                return e;
+            } else if (value instanceof BigDecimal && ((BigDecimal) e.getValue()).compareTo((BigDecimal) value) == 0) {
+                // 结果是:-1 小于,0 等于,1 大于
+                return e;
+            } else if (String.valueOf(e.getValue()).equals(String.valueOf(value))) {
+                // 其他类型
                 return e;
             }
         }
