47a5217660efd3d6e6b0b1054744fa7df8af9c65
Caratacus <330952486@qq.com>
2017-12-07 15:03:32 +0800

    使用transient关键字去除Page中部分字段参与序列化

--- mybatis-plus-support/src/main/java/com/baomidou/mybatisplus/plugins/Page.java
+++ mybatis-plus-support/src/main/java/com/baomidou/mybatisplus/plugins/Page.java
@@ -41,7 +41,7 @@ public class Page<T> extends Pagination {
     /**
      * 查询参数
      */
-    private Map<String, Object> condition;
+    private transient Map<String, Object> condition;
 
     public Page() {
         /* 注意，传入翻页参数 */
