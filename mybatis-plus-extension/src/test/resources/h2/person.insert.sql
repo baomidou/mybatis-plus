c20b4b073c2f6d002376a181d1e3179a6a17d519
yuxiaobin <179634696@qq.com>
2018-03-23 13:46:57 +0800

    驼峰命名字段 - 测试用例

---
+++ mybatis-plus-extension/src/test/resources/h2/person.insert.sql
@@ -0,0 +1,5 @@
+insert into h2person(testId, name, testDate, age, price, testType, version) values (101,'Tomcat', '2017-1-1 1:1:1', 3, 9.99, 1, 1),
+insert into h2person(testId, name, testDate, age, price, testType, version) values (102,'Jerry', '2017-3-1 1:1:1', 2, 19.99, 2, 2);
+insert into h2person(testId, name, testDate, age, price, testType, version) values (103,'Bob', '2017-4-1 1:1:1', 13, 99.99, 3, 3);
+insert into h2person(testId, name, testDate, age, price, testType, version) values (104,'Joe', '2017-2-1 1:1:1', 18, 1.99, 1, 4);
+insert into h2person(testId, name, testDate, age, price, testType, version) values (105,'Tony', '2017-2-1 1:1:1', 18, 1.99, 1, 5);
