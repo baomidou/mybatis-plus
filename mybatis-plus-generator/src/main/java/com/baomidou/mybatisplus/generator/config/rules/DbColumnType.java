30f1b02d39e9724814ff56aed8e350ffbcc50dfb
miemie <wonderming@vip.qq.com>
2018-05-23 20:13:58 +0800

    微调

--- mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/config/rules/DbColumnType.java
+++ mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/config/rules/DbColumnType.java
@@ -28,40 +28,46 @@ import lombok.Getter;
 @Getter
 public enum DbColumnType {
     // 基本类型
+    BASE_BYTE("byte", null),
+    BASE_SHORT("short", null),
+    BASE_CHAR("char", null),
     BASE_INT("int", null),
     BASE_LONG("long", null),
-    BASE_CHAR("char", null),
-    BASE_BYTE("byte", null),
-    BASE_BOOLEAN("boolean", null),
-    BASE_SHORT("short", null),
     BASE_FLOAT("float", null),
     BASE_DOUBLE("double", null),
+    BASE_BOOLEAN("boolean", null),
 
     // 包装类型
-    STRING("String", null),
-    LONG("Long", null),
+    BYTE("Byte", null),
+    SHORT("Short", null),
+    CHARACTER("Character", null),
     INTEGER("Integer", null),
+    LONG("Long", null),
     FLOAT("Float", null),
     DOUBLE("Double", null),
     BOOLEAN("Boolean", null),
-    BYTE("Byte", null),
-    BYTE_ARRAY("byte[]", null),
-    CHARACTER("Character", null),
-    OBJECT("Object", null),
-    DATE("Date", "java.util.Date"),
+    STRING("String", null),
+
+    // sql 包下数据类型
     DATE_SQL("Date", "java.sql.Date"),
     TIME("Time", "java.sql.Time"),
+    TIMESTAMP("Timestamp", "java.sql.Timestamp"),
     BLOB("Blob", "java.sql.Blob"),
     CLOB("Clob", "java.sql.Clob"),
-    TIMESTAMP("Timestamp", "java.sql.Timestamp"),
-    BIG_INTEGER("BigInteger", "java.math.BigInteger"),
-    BIG_DECIMAL("BigDecimal", "java.math.BigDecimal"),
 
     // java8 新时间类型
     LOCAL_DATE("LocalDate", "java.time.LocalDate"),
     LOCAL_TIME("LocalTime", "java.time.LocalTime"),
     YEAR("Year", "java.time.Year"),
-    LOCAL_DATE_TIME("LocalDateTime", "java.time.LocalDateTime");
+    YEAR_MONTH("YearMonth", "java.time.YearMonth"),
+    LOCAL_DATE_TIME("LocalDateTime", "java.time.LocalDateTime"),
+
+    // 其他杂类
+    BYTE_ARRAY("byte[]", null),
+    OBJECT("Object", null),
+    DATE("Date", "java.util.Date"),
+    BIG_INTEGER("BigInteger", "java.math.BigInteger"),
+    BIG_DECIMAL("BigDecimal", "java.math.BigDecimal");
 
     /**
      * 类型
