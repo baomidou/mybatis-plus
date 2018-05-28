ded17ee6a540ff9df75c1a0c2ac2a3e58c6d446e
hubin <jobob@qq.com>
2018-05-28 18:03:48 +0800

    删除不再需要的测试用例

---
+++ mybatis-plus-core/src/test/java/com/baomidou/mybatisplus/core/test/SqlConditionTest.java
@@ -0,0 +1,33 @@
+package com.baomidou.mybatisplus.core.test;
+
+import org.junit.Test;
+
+import com.baomidou.mybatisplus.core.conditions.EntityWrapper;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+
+public class SqlConditionTest {
+
+    private void log(String message) {
+        System.out.println(message);
+    }
+
+    @Test
+    public void test() {
+        Wrapper wrapper = new EntityWrapper().eq("abc", 123);
+        log(wrapper.getSqlSegment());
+    }
+
+//    public void test() {
+//        String sql = new SqlCondition()
+//            .where("b.age > 18", condition ->
+//                condition.and("b.type = 'rabid'")
+//                    .or(nested -> nested.apply("name='12'").and("age=1"))
+//                    .notIn("ads,2112,212")
+//                    .last("LIMIT 1")
+//            ).sqlSegment();
+//
+//        log(sql);
+//        assertEquals("WHERE b.age > 18 AND b.type = 'rabid' OR ( name='12' AND age=1 ) NOT IN ( ads,2112,212 ) LIMIT 1", sql);
+//    }
+
+}
--- mybatis-plus-core/src/test/java/com/baomidou/mybatisplus/core/test/query/ISqlQueue.java
+++
@@ -1,8 +0,0 @@
-package com.baomidou.mybatisplus.core.test.query;
-
-
-public interface ISqlQueue<T> {
-
-   void enqueue(T element);
-
-}
