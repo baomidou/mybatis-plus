3fa5f084d699a58b71a58c097cb499342c0862fc
miemie <wonderming@vip.qq.com>
2018-06-07 20:48:16 +0800

    去掉多余的导入包

--- mybatis-plus-core/src/test/java/com/baomidou/mybatisplus/core/test/Main.java
+++ mybatis-plus-core/src/test/java/com/baomidou/mybatisplus/core/test/Main.java
@@ -1,9 +1,5 @@
 package com.baomidou.mybatisplus.core.test;
 
-import com.baomidou.mybatisplus.core.metadata.TableInfo;
-import com.baomidou.mybatisplus.core.toolkit.TableInfoHelper;
-import org.junit.Assert;
-
 import java.time.LocalDate;
 
 public class Main {
@@ -13,22 +9,11 @@ public class Main {
     }
 
     public static void query() {
-        Query query = new Query(User.class)
-            .select(User::getId, User::getName)
-            .eq(User::getId, 1)
-            .eq(User::getName, "张三")
-            .orderBy(User::getName);
-        System.out.println(query);
-    }
-
-    public static void columnPrefix() {
-        TableInfo tableInfo = TableInfoHelper.initTableInfo(null, Test.class);
-        Assert.assertEquals("test_id", tableInfo.getKeyColumn());
-        tableInfo.getFieldList().forEach(i-> System.out.println(i.getColumn()));
     }
 
 
     public static class Test {
+
         private Long id;
         private String name;
         private String age;
