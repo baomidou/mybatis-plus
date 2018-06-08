ea551dd3370b7cb8265b897de1948fb424efd2db
miemie <wonderming@vip.qq.com>
2018-06-08 13:35:36 +0800

    添加注释

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/mapper/BaseMapper.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/mapper/BaseMapper.java
@@ -125,7 +125,7 @@ public interface BaseMapper<T> {
      *
      * @param entity        实体对象 (set 条件值)
      * @param updateWrapper 实体对象封装操作类（可以为 null）
-     * @return
+     * @return int
      */
     Integer update(@Param("et") T entity, @Param("ew") Wrapper<T> updateWrapper);
 
