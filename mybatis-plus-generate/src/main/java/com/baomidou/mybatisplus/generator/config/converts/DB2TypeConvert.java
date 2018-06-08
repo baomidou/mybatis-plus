9bf53a8e3ad04588672b189755fb2393f62ea4a2
yao <yao.zhan@yilutong.com>
2018-06-08 10:06:14 +0800

    修改DB2字段转换未调用DB2TypeConvert的方法，以及主键不指定的注解

--- mybatis-plus-generate/src/main/java/com/baomidou/mybatisplus/generator/config/converts/DB2TypeConvert.java
+++ mybatis-plus-generate/src/main/java/com/baomidou/mybatisplus/generator/config/converts/DB2TypeConvert.java
@@ -30,28 +30,35 @@ public class DB2TypeConvert implements ITypeConvert {
 
     @Override
     public DbColumnType processTypeConvert(String fieldType) {
-        String t = fieldType.toUpperCase();
-        if (t.contains("CHAR")) {
+        String t = fieldType.toLowerCase();
+    	if (t.contains("char") || t.contains("text")) {
             return DbColumnType.STRING;
-        } else if (t.contains("DATE") || t.contains("TIMESTAMP")) {
+        } else if (t.contains("bigint")) {
+            return DbColumnType.LONG;
+        } else if (t.contains("smallint")) {
+            return DbColumnType.BASE_SHORT;
+        }else if (t.contains("int")) {
+            return DbColumnType.INTEGER;
+        } else if (t.contains("date") || t.contains("time") || t.contains("year") || t.contains("timestamp")) {
             return DbColumnType.DATE;
-        } else if (t.contains("NUMBER")) {
-            if (t.matches("NUMBER\\(+\\d\\)")) {
-                return DbColumnType.INTEGER;
-            } else if (t.matches("NUMBER\\(+\\d{2}+\\)")) {
-                return DbColumnType.LONG;
-            }
-            return DbColumnType.DOUBLE;
-        } else if (t.contains("FLOAT")) {
-            return DbColumnType.FLOAT;
+        } else if (t.contains("text")) {
+            return DbColumnType.STRING;
+        } else if (t.contains("bit")) {
+            return DbColumnType.BOOLEAN;
+        } else if (t.contains("decimal")) {
+            return DbColumnType.BIG_DECIMAL;
         } else if (t.contains("clob")) {
             return DbColumnType.CLOB;
-        } else if (t.contains("BLOB")) {
-            return DbColumnType.OBJECT;
+        } else if (t.contains("blob")) {
+            return DbColumnType.BLOB;
         } else if (t.contains("binary")) {
             return DbColumnType.BYTE_ARRAY;
-        } else if (t.contains("RAW")) {
-            return DbColumnType.BYTE_ARRAY;
+        } else if (t.contains("float")) {
+            return DbColumnType.FLOAT;
+        } else if (t.contains("double")) {
+            return DbColumnType.DOUBLE;
+        } else if (t.contains("json") || t.contains("enum")) {
+            return DbColumnType.STRING;
         }
         return DbColumnType.STRING;
     }
