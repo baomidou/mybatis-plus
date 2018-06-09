1c5e1b06ba0e46bea9379925b5c2d8ef715ef542
hubin <jobob@qq.com>
2018-06-09 14:38:15 +0800

    添加注释和重载类说明

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/override/PageMapperProxyFactory.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/override/PageMapperProxyFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2011-2020, hubin (jobob@qq.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.baomidou.mybatisplus.core.override;
 
 import java.lang.reflect.Method;
@@ -8,9 +23,13 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.apache.ibatis.session.SqlSession;
 
 /**
- * 替换掉引用
+ * <p>
+ * 替换掉引用<br/>
+ * 重写类： org.apache.ibatis.binding.MapperProxyFactory
+ * </p>
+ *
  * @author ming
- * @since 2018/6/9
+ * @since 2018-06-09
  */
 public class PageMapperProxyFactory<T> {
 
---
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/override/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <p>
+ * Page 部分重构相关逻辑类
+ * </p>
+ *
+ * @author hubin
+ * @since 2018-06-09
+ */
+package com.baomidou.mybatisplus.core.override;
