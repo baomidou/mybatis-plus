ffa2d9cf61322f4658e6087dc0c99c9dbef3ba71
hubin <jobob@qq.com>
2018-06-28 09:42:56 +0800

    暂时先删除适配模块

--- mybatis-plus-adapter/src/main/java/com/baomidou/mybatisplus/mapper/BaseMapper.java
+++
@@ -1,9 +0,0 @@
-package com.baomidou.mybatisplus.mapper;
-
-/**
- * 请尽快修改该类的引用 mp 3.1 将删除该类
- */
-@Deprecated
-public interface BaseMapper extends com.baomidou.mybatisplus.core.mapper.BaseMapper {
-
-}
