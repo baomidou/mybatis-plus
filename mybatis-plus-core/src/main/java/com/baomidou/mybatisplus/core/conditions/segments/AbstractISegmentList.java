aca5c83d276a8e64baafd13faa49bc157672332d
miemie <wonderming@vip.qq.com>
2018-06-28 16:56:47 +0800

    加上声明

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/conditions/segments/AbstractISegmentList.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/conditions/segments/AbstractISegmentList.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2011-2020, hubin (jobob@qq.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.baomidou.mybatisplus.core.conditions.segments;
 
 import java.util.ArrayList;
@@ -7,8 +22,12 @@ import java.util.List;
 import com.baomidou.mybatisplus.core.conditions.ISqlSegment;
 
 /**
+ * <p>
+ * SQL 片段集合 处理的抽象类
+ * </p>
+ *
  * @author miemie
- * @since 2018-06-28
+ * @since 2018-06-27
  */
 @SuppressWarnings("serial")
 public abstract class AbstractISegmentList extends ArrayList<ISqlSegment> implements ISqlSegment {
