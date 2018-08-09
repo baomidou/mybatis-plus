3b271dc3671689fe2a6edd9cfb056aa1797fb96c
miemie <wonderming@vip.qq.com>
2018-08-09 22:17:22 +0800

    测试json反序列化为page

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/SqlTest.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/SqlTest.java
@@ -1,14 +1,21 @@
 package com.baomidou.mybatisplus.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SqlTest {
 
+    private final ObjectMapper mapper = new ObjectMapper();
+
     @Test
-    public void test() {
-        Stream.of(1, 2, 3, 4, 5).collect(Collectors.toList()).toArray();
+    public void testPageJsonDecode() throws Exception {
+        String json = "{\"current\":2,\"size\":9,\"ascs\":[\"name\",\"age\",\"qiuqiu\"]}";
+        Page page = mapper.readValue(json, Page.class);
+        Assert.assertEquals(2, page.getCurrent());
+        Assert.assertEquals(9, page.getSize());
+        Assert.assertEquals(3, page.ascs().length);
     }
 }
