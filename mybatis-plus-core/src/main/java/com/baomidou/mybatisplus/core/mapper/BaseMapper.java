acc7d76bfa54e6a5b96f4b95f7240311c7e23378
miemie <wonderming@vip.qq.com>
2018-08-18 14:11:52 +0800

    baseMapper 的 insert 和 update 返回值不再使用包装类

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/mapper/BaseMapper.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/mapper/BaseMapper.java
@@ -15,16 +15,15 @@
  */
 package com.baomidou.mybatisplus.core.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import org.apache.ibatis.annotations.Param;
+
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.ibatis.annotations.Param;
-
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.core.toolkit.Constants;
 
 /*
 
@@ -94,7 +93,7 @@ public interface BaseMapper<T> {
      *
      * @param entity 实体对象
      */
-    Integer insert(T entity);
+    int insert(T entity);
 
     /**
      * <p>
@@ -103,7 +102,7 @@ public interface BaseMapper<T> {
      *
      * @param id 主键ID
      */
-    Integer deleteById(Serializable id);
+    int deleteById(Serializable id);
 
     /**
      * <p>
@@ -112,7 +111,7 @@ public interface BaseMapper<T> {
      *
      * @param columnMap 表字段 map 对象
      */
-    Integer deleteByMap(@Param(Constants.COLUMN_MAP) Map<String, Object> columnMap);
+    int deleteByMap(@Param(Constants.COLUMN_MAP) Map<String, Object> columnMap);
 
     /**
      * <p>
@@ -121,7 +120,7 @@ public interface BaseMapper<T> {
      *
      * @param queryWrapper 实体对象封装操作类（可以为 null）
      */
-    Integer delete(@Param(Constants.WRAPPER) Wrapper<T> queryWrapper);
+    int delete(@Param(Constants.WRAPPER) Wrapper<T> queryWrapper);
 
     /**
      * <p>
@@ -130,7 +129,7 @@ public interface BaseMapper<T> {
      *
      * @param idList 主键ID列表(不能为 null 以及 empty)
      */
-    Integer deleteBatchIds(@Param(Constants.COLLECTION) Collection<? extends Serializable> idList);
+    int deleteBatchIds(@Param(Constants.COLLECTION) Collection<? extends Serializable> idList);
 
     /**
      * <p>
@@ -139,7 +138,7 @@ public interface BaseMapper<T> {
      *
      * @param entity 实体对象
      */
-    Integer updateById(@Param(Constants.ENTITY) T entity);
+    int updateById(@Param(Constants.ENTITY) T entity);
 
     /**
      * <p>
@@ -149,7 +148,7 @@ public interface BaseMapper<T> {
      * @param entity        实体对象 (set 条件值,不能为 null)
      * @param updateWrapper 实体对象封装操作类（可以为 null,里面的 entity 用于生成 where 语句）
      */
-    Integer update(@Param(Constants.ENTITY) T entity, @Param(Constants.WRAPPER) Wrapper<T> updateWrapper);
+    int update(@Param(Constants.ENTITY) T entity, @Param(Constants.WRAPPER) Wrapper<T> updateWrapper);
 
     /**
      * <p>
