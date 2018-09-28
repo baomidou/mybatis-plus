a4cdb520e07bb8691865dfea90601ebabf3831be
hubin <jobob@qq.com>
2018-09-28 23:19:31 +0800

    发布 3.0.4

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/h2/H2StudentMapperTest.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/h2/H2StudentMapperTest.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Objects;
 
 import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.baomidou.mybatisplus.core.conditions.Condition;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.test.h2.entity.enums.GenderEnum;
 import com.baomidou.mybatisplus.test.h2.entity.enums.GradeEnum;
 import com.baomidou.mybatisplus.test.h2.entity.mapper.H2StudentMapper;
@@ -62,4 +67,11 @@ public class H2StudentMapperTest extends BaseTest {
 
     }
 
+    @Test
+    public void pageCountZeroTest() {
+        IPage page = studentMapper.selectPage(new Page<>(), Condition.<H2Student>create().eq("name", "无"));
+        if (null != page) {
+            System.out.println("total: " + page.getTotal());
+        }
+    }
 }
