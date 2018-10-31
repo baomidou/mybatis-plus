13f1ea0b851ad29968e0a3afdcb086cb6888cca3
miemie <wonderming@vip.qq.com>
2018-10-31 13:56:45 +0800

    更新分页动态参数需要的相关模型

--- mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/plugins/pagination/DialectModel.java
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/plugins/pagination/DialectModel.java
@@ -1,17 +1,25 @@
 package com.baomidou.mybatisplus.extension.plugins.pagination;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 import org.apache.ibatis.mapping.ParameterMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
+ * 分页参数动态化所需 model
+ *
  * @author miemie
  * @since 2018-10-31
  */
 @Data
+@Accessors(chain = true)
 public class DialectModel {
+    public static final String OFFSET_NAME = "mybatis_plus_offset";
+    public static final String LIMIT_NAME = "mybatis_plus_limit";
 
     /**
      * 分页方言 sql
@@ -27,4 +35,14 @@ public class DialectModel {
      */
     private Consumer<List<ParameterMapping>> limitConsumer = i -> {
     };
+    /**
+     * 分页数据参数 map
+     */
+    private Map<String, Long> dialectMap = new HashMap<>(2);
+
+    public DialectModel putToDialectMap(long offset, long limit) {
+        dialectMap.put(OFFSET_NAME, offset);
+        dialectMap.put(LIMIT_NAME, limit);
+        return this;
+    }
 }
