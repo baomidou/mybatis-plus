59a2539ace4f4b6d4237f25d0a7fc387ee7a0f77
nieqiurong <nieqiurong@163.com>
2018-11-09 16:54:00 +0800

    强制使用自定义ParameterHandler.

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/MybatisXMLLanguageDriver.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/MybatisXMLLanguageDriver.java
@@ -15,7 +15,6 @@
  */
 package com.baomidou.mybatisplus;
 
-import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.scripting.xmltags.XMLLanguageDriver;
@@ -31,7 +30,7 @@ import org.apache.ibatis.scripting.xmltags.XMLLanguageDriver;
 public class MybatisXMLLanguageDriver extends XMLLanguageDriver {
 
     @Override
-    public ParameterHandler createParameterHandler(MappedStatement mappedStatement, Object parameterObject,
+    public MybatisDefaultParameterHandler createParameterHandler(MappedStatement mappedStatement, Object parameterObject,
                                                    BoundSql boundSql) {
         /* 使用自定义 ParameterHandler */
         return new MybatisDefaultParameterHandler(mappedStatement, parameterObject, boundSql);
