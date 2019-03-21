1d289c9e0e0fc9bf05df9438480fc777e1b5ba2f
miemie <wonderming@vip.qq.com>
2019-03-21 18:34:31 +0800

    测试

--- mybatis-plus-extension/src/test/java/com/baomidou/mybatisplus/extension/handlers/MybatisMapWrapperTest.java
+++ mybatis-plus-extension/src/test/java/com/baomidou/mybatisplus/extension/handlers/MybatisMapWrapperTest.java
@@ -10,13 +10,20 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author miemie
  * @since 2019-03-21
  */
-public class MybatisMapWrapperTest {
+class MybatisMapWrapperTest {
 
     private final MybatisMapWrapper mapWrapper = new MybatisMapWrapper(null, Collections.emptyMap());
 
     @Test
-    public void findPropertyOfTrue() {
-        boolean useCamelCaseMapping = true;
-        assertThat(mapWrapper.findProperty("xxx", useCamelCaseMapping));
+    void findProperty() {
+        assertThat(mapWrapper.findProperty("xxx", true)).isEqualTo("xxx");
+        assertThat(mapWrapper.findProperty("xxx_sss", true)).isEqualTo("xxxSss");
+        assertThat(mapWrapper.findProperty("xxx_sss_eee", true)).isEqualTo("xxxSssEee");
+        assertThat(mapWrapper.findProperty("XXX_SSS_EEE", true)).isEqualTo("xxxSssEee");
+        assertThat(mapWrapper.findProperty("xxxSss", true)).isEqualTo("xxxSss");
+        /* 注意一下情况不支持 */
+
+        // 1.不包含下划线,并且首字母大写,会被全部转成小写
+        assertThat(mapWrapper.findProperty("SxxSss", true)).isEqualTo("sxxsss");
     }
 }
