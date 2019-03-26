81177f165819e81ad42043d3831dbb9e36000d84
miemie <wonderming@vip.qq.com>
2019-03-26 14:35:22 +0800

    注释

--- mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/service/IService.java
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/service/IService.java
@@ -343,8 +343,24 @@ public interface IService<T> {
     BaseMapper<T> getBaseMapper();
 
     /**
+     * 以下的方法使用介绍:
+     *
+     * 一. 名称介绍
+     * 1. 方法名带有 query 的为对数据的查询操作, 方法名带有 update 的为对数据的修改操作
+     * 2. 方法名带有 lambda 的为内部方法入参 column 支持函数式的
+     *
+     * 二. 支持介绍
+     * 1. 方法名带有 query 的支持以 {@link ChainQuery} 内部的方法名结尾进行数据查询操作
+     * 2. 方法名带有 update 的支持以 {@link ChainUpdate} 内部的方法名为结尾进行数据修改操作
+     *
+     * 三. 使用示例,只用不带 lambda 的方法各展示一个例子,其他类推
+     * 1. 根据条件获取一条数据: `query().eq("column", value).one()`
+     * 2. 根据条件删除一条数据: `update().eq("column", value).remove()`
+     *
+     */
+
+    /**
      * 链式查询 普通
-     * 支持 {@link ChainQuery} 内的方法为结尾进行查询
      *
      * @return QueryWrapper 的包装类
      */
@@ -364,7 +380,6 @@ public interface IService<T> {
 
     /**
      * 链式更改 普通
-     * 支持 {@link ChainUpdate} 内的方法为结尾进行数据修改
      *
      * @return UpdateWrapper 的包装类
      */
