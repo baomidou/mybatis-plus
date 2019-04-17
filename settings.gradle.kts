9ac438fe7ebaaa7151406344e38ae22f2ec8029b
nieqiuqiu <nieqiurong@163.com>
2019-04-17 14:56:03 +0800

    还原.

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "mybatis-plus-root"
-
-include(":mybatis-plus")
-include(":mybatis-plus-core")
-include(":mybatis-plus-annotation")
-include(":mybatis-plus-extension")
-include(":mybatis-plus-generator")
-include(":mybatis-plus-boot-starter")
