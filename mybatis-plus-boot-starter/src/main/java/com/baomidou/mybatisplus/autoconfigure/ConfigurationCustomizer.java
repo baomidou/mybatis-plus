6209536e6c8a8de106167f4c198af1914e3ee083
miemie <wonderming@vip.qq.com>
2019-05-04 20:47:24 +0800

    ConfigurationCustomizer 内部方法入参更新为 MybatisConfiguration

--- mybatis-plus-boot-starter/src/main/java/com/baomidou/mybatisplus/autoconfigure/ConfigurationCustomizer.java
+++ mybatis-plus-boot-starter/src/main/java/com/baomidou/mybatisplus/autoconfigure/ConfigurationCustomizer.java
@@ -15,10 +15,10 @@
  */
 package com.baomidou.mybatisplus.autoconfigure;
 
-import org.apache.ibatis.session.Configuration;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
 
 /**
- * Callback interface that can be customized a {@link Configuration} object generated on auto-configuration.
+ * Callback interface that can be customized a {@link MybatisConfiguration} object generated on auto-configuration.
  *
  * @author Kazuki Shimizu
  * @since 1.2.1
@@ -27,8 +27,9 @@ import org.apache.ibatis.session.Configuration;
 public interface ConfigurationCustomizer {
 
     /**
-     * Customize the given a {@link Configuration} object.
+     * Customize the given a {@link MybatisConfiguration} object.
+     *
      * @param configuration the configuration object to customize
      */
-    void customize(Configuration configuration);
+    void customize(MybatisConfiguration configuration);
 }
