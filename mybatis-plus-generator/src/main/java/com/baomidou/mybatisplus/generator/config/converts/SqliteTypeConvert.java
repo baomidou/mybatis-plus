4c0fccfeb679887da99b6fa11bb4c105ecbeb867
miemie <wonderming@vip.qq.com>
2019-06-03 17:16:16 +0800

    发布 3.1.2.3-SNAPSHOT

--- mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/config/converts/SqliteTypeConvert.java
+++ mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/config/converts/SqliteTypeConvert.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2011-2020, baomidou (jobob@qq.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.baomidou.mybatisplus.generator.config.converts;
 
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
@@ -14,7 +29,7 @@ public class SqliteTypeConvert implements ITypeConvert {
     @Override
     public IColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {
         String t = fieldType.toLowerCase();
-       if (t.contains("bigint")) {
+        if (t.contains("bigint")) {
             return DbColumnType.LONG;
         } else if (t.contains("tinyint(1)") || t.contains("boolean")) {
             return DbColumnType.BOOLEAN;
@@ -27,8 +42,8 @@ public class SqliteTypeConvert implements ITypeConvert {
         } else if (t.contains("clob")) {
             return DbColumnType.CLOB;
         } else if (t.contains("blob")) {
-           return DbColumnType.BLOB;
-       } else if (t.contains("float")) {
+            return DbColumnType.BLOB;
+        } else if (t.contains("float")) {
             return DbColumnType.FLOAT;
         } else if (t.contains("double")) {
             return DbColumnType.DOUBLE;
