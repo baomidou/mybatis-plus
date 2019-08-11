0e48f73d3e6933a461097abd218991dd06b33939
miemie <wonderming@vip.qq.com>
2019-08-11 23:27:42 +0800

    移除 PerformanceInterceptor 相关,建议使用p6spy

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/toolkit/sql/SqlUtils.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/toolkit/sql/SqlUtils.java
@@ -26,22 +26,6 @@ import com.baomidou.mybatisplus.core.toolkit.StringPool;
  */
 public class SqlUtils {
 
-    private final static SqlFormatter SQL_FORMATTER = new SqlFormatter();
-
-    /**
-     * 格式sql
-     */
-    @Deprecated
-    public static String sqlFormat(String boundSql, boolean format) {
-        if (format) {
-            try {
-                return SQL_FORMATTER.format(boundSql);
-            } catch (Exception ignored) {
-            }
-        }
-        return boundSql;
-    }
-
     /**
      * 用%连接like
      *
