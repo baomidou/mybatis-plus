440bb5bc37741c3458a078b547a446679365298a
yuxiaobin <179634696@qq.com>
2019-12-04 13:25:58 +0800

    fix: pull#1895

--- mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/plugins/pagination/dialects/Oracle12cDialect.java
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/plugins/pagination/dialects/Oracle12cDialect.java
@@ -27,7 +27,6 @@ public class Oracle12cDialect implements IDialect {
 
     @Override
     public DialectModel buildPaginationSql(String originalSql, long offset, long limit) {
-        limit = (offset >= 1) ? (offset + limit) : limit;
         String sql = originalSql + " OFFSET " + FIRST_MARK + " ROWS FETCH NEXT " + SECOND_MARK + " ROWS ONLY ";
         return new DialectModel(sql, offset, limit).setConsumerChain();
     }
