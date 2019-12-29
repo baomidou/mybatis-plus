93fff0da4e4e9e5ec7f31ee945efc3ca701e4dc2
nieqiuqiu <nieqiurong@163.com>
2019-12-29 22:21:43 +0800

    test

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/h2/cache/mapper/CacheMapper.java
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/h2/cache/mapper/CacheMapper.java
@@ -16,10 +16,11 @@
 package com.baomidou.mybatisplus.test.h2.cache.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.test.h2.cache.CustomCache;
 import com.baomidou.mybatisplus.test.h2.cache.model.CacheModel;
 import org.apache.ibatis.annotations.CacheNamespace;
 
-@CacheNamespace
+@CacheNamespace(implementation = CustomCache.class)
 public interface CacheMapper extends BaseMapper<CacheModel> {
 
 }
