62201a5b0e9834a5a9f21c6ed17c1bcc16197df9
聂秋秋 <nieqiurong@163.com>
2020-01-30 17:50:30 +0800

    微软数据库代码生成器表过滤支持.
    https://github.com/baomidou/mybatis-plus/issues/2102

--- mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/config/querys/SqlServerQuery.java
+++ mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/config/querys/SqlServerQuery.java
@@ -25,13 +25,13 @@ import java.sql.SQLException;
  * @since 2018-01-16
  */
 public class SqlServerQuery extends AbstractDbQuery {
-
+    
     @Override
     public String tablesSql() {
-        return "select cast(so.name as varchar(500)) as TABLE_NAME, " +
+        return "select * from (select cast(so.name as varchar(500)) as TABLE_NAME, " +
             "cast(sep.value as varchar(500)) as COMMENTS from sysobjects so " +
             "left JOIN sys.extended_properties sep on sep.major_id=so.id and sep.minor_id=0 " +
-            "where (xtype='U' or xtype='v')";
+            "where (xtype='U' or xtype='v')) a where 1=1 ";
     }
 
 
