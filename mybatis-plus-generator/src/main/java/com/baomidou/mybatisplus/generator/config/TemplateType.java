605e63747bab6cc66d0c58aeec4f13abda37510f
聂秋秋 <nieqiurong@163.com>
2020-04-28 11:27:41 +0800

    增加生成器禁用模板生成.
    https://github.com/baomidou/mybatis-plus/issues/2384
    
    https://gitee.com/baomidou/mybatis-plus/issues/I1BRRV

---
+++ mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/config/TemplateType.java
@@ -0,0 +1,13 @@
+package com.baomidou.mybatisplus.generator.config;
+
+/**
+ * 模板类型
+ *
+ * @author nieqiurong 2020/4/28.
+ * @since 3.3.2
+ */
+public enum TemplateType {
+    
+    ENTITY, SERVICE, CONTROLLER, MAPPER, XML
+    
+}
