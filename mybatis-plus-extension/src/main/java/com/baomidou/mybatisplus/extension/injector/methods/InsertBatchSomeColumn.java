ebbce007f451840f86b2f06eba3d2442f4ff3f3a
miemie <wonderming@vip.qq.com>
2020-06-08 12:33:45 +0800

    code style

--- mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/injector/methods/InsertBatchSomeColumn.java
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/injector/methods/InsertBatchSomeColumn.java
@@ -21,7 +21,6 @@ import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.metadata.TableFieldInfo;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
 import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.sql.SqlScriptUtils;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -88,7 +87,7 @@ public class InsertBatchSomeColumn extends AbstractMethod {
         String keyProperty = null;
         String keyColumn = null;
         // 表包含主键处理逻辑,如果不包含主键当普通字段处理
-        if (StringUtils.isNotBlank(tableInfo.getKeyProperty())) {
+        if (tableInfo.havePK()) {
             if (tableInfo.getIdType() == IdType.AUTO) {
                 /* 自增主键 */
                 keyGenerator = new Jdbc3KeyGenerator();
