dfcd657436138a17142b59d419db375ecd6f7102
miemie <wonderming@vip.qq.com>
2020-06-09 16:38:52 +0800

    MybatisDefaultParameterHandler deprecated

---
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/MybatisDefaultParameterHandler.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2011-2020, baomidou (jobob@qq.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.baomidou.mybatisplus.core;
+
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+
+/**
+ * @author miemie
+ * @since 2020-06-09
+ * @deprecated see {@link MybatisParameterHandler}
+ */
+@Deprecated
+public class MybatisDefaultParameterHandler extends MybatisParameterHandler {
+
+    public MybatisDefaultParameterHandler(MappedStatement mappedStatement, Object parameter, BoundSql boundSql) {
+        super(mappedStatement, parameter, boundSql);
+    }
+}
