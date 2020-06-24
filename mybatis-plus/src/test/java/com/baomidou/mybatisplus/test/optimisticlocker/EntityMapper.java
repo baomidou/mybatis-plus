d9f69d59c90de716f17699efd90cf70242d23d2a
miemie <wonderming@vip.qq.com>
2020-06-24 14:09:58 +0800

    test

---
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/optimisticlocker/EntityMapper.java
@@ -0,0 +1,10 @@
+package com.baomidou.mybatisplus.test.optimisticlocker;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author miemie
+ * @since 2020-06-24
+ */
+public interface EntityMapper extends BaseMapper<Entity> {
+}
