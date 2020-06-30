9b3fc9869ba4bbd49323221a116acf9ad2ed0a8b
miemie <wonderming@vip.qq.com>
2020-06-30 11:10:42 +0800

    清理 DbType.java 过时属性

--- mybatis-plus-annotation/src/main/java/com/baomidou/mybatisplus/annotation/DbType.java
+++ mybatis-plus-annotation/src/main/java/com/baomidou/mybatisplus/annotation/DbType.java
@@ -19,7 +19,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * MybatisPlus 数据库类型
+ * MybatisPlus 支持的数据库类型,主要用于分页方言
  *
  * @author hubin
  * @since 2018-06-23
@@ -31,85 +31,85 @@ public enum DbType {
     /**
      * MYSQL
      */
-    MYSQL("mysql", "MySql数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.MySqlDialect"),
+    MYSQL("mysql", "MySql数据库"),
     /**
      * MARIADB
      */
-    MARIADB("mariadb", "MariaDB数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.MariaDBDialect"),
+    MARIADB("mariadb", "MariaDB数据库"),
     /**
      * ORACLE
      */
-    ORACLE("oracle", "Oracle11g及以下数据库(高版本推荐使用ORACLE_NEW)", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.OracleDialect"),
+    ORACLE("oracle", "Oracle11g及以下数据库(高版本推荐使用ORACLE_NEW)"),
     /**
      * oracle12c new pagination
      */
-    ORACLE_12C("oracle12c", "Oracle12c+数据库","com.baomidou.mybatisplus.extension.plugins.pagination.dialects.Oracle12cDialect"),
+    ORACLE_12C("oracle12c", "Oracle12c+数据库"),
 
     /**
      * DB2
      */
-    DB2("db2", "DB2数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.DB2Dialect"),
+    DB2("db2", "DB2数据库"),
     /**
      * H2
      */
-    H2("h2", "H2数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.H2Dialect"),
+    H2("h2", "H2数据库"),
     /**
      * HSQL
      */
-    HSQL("hsql", "HSQL数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.HSQLDialect"),
+    HSQL("hsql", "HSQL数据库"),
     /**
      * SQLITE
      */
-    SQLITE("sqlite", "SQLite数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.SQLiteDialect"),
+    SQLITE("sqlite", "SQLite数据库"),
     /**
      * POSTGRE
      */
-    POSTGRE_SQL("postgresql", "Postgre数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.PostgreDialect"),
+    POSTGRE_SQL("postgresql", "Postgre数据库"),
     /**
      * SQLSERVER2005
      */
-    SQL_SERVER2005("sqlserver2005", "SQLServer2005数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.SQLServer2005Dialect"),
+    SQL_SERVER2005("sqlserver2005", "SQLServer2005数据库"),
     /**
      * SQLSERVER
      */
-    SQL_SERVER("sqlserver", "SQLServer数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.SQLServerDialect"),
+    SQL_SERVER("sqlserver", "SQLServer数据库"),
     /**
      * DM
      */
-    DM("dm", "达梦数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.DmDialect"),
+    DM("dm", "达梦数据库"),
     /**
      * xugu
      */
-    XU_GU("xugu", "虚谷数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.XuGuDialect"),
+    XU_GU("xugu", "虚谷数据库"),
     /**
      * Kingbase
      */
-    KINGBASE_ES("kingbasees", "人大金仓数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.KingbaseDialect"),
+    KINGBASE_ES("kingbasees", "人大金仓数据库"),
 
     /**
      * Phoenix
      */
-    PHOENIX("phoenix", "Phoenix HBase数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.PhoenixDialect"),
+    PHOENIX("phoenix", "Phoenix HBase数据库"),
 
     /**
      * Gauss
      */
-    GAUSS("zenith", "Gauss 数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.GaussDialect"),
+    GAUSS("zenith", "Gauss 数据库"),
 
     /**
      * clickhouse
      */
-    CLICKHOUSE("clickhouse", "clickhouse 数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.ClickhouseDialect"),
+    CLICKHOUSE("clickhouse", "clickhouse 数据库"),
 
     /**
      * GBase
      */
-    GBASE("gbase", "南大通用数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.GBaseDialect"),
+    GBASE("gbase", "南大通用数据库"),
 
     /**
      * UNKONWN DB
      */
-    OTHER("other", "其他数据库", "com.baomidou.mybatisplus.extension.plugins.pagination.dialects.UnknownDialect");
+    OTHER("other", "其他数据库");
 
     /**
      * 数据库名称
@@ -119,14 +119,6 @@ public enum DbType {
      * 描述
      */
     private final String desc;
-
-    /**
-     * 分页方言
-     *
-     * @deprecated 3.3.1
-     */
-    @Deprecated
-    private String dialect;
 
     /**
      * 获取数据库类型
@@ -140,6 +132,5 @@ public enum DbType {
             }
         }
         return OTHER;
-
     }
 }
