ba0f70acac9bb6f34b23b60169d33827d365f97a
miemie <wonderming@vip.qq.com>
2020-07-16 15:22:53 +0800

    test to douhao

---
+++ mybatis-plus-extension/src/test/java/com/baomidou/mybatisplus/extension/plugins/inner/DynamicTableNameInnerInterceptorTest.java
@@ -0,0 +1,15 @@
+package com.baomidou.mybatisplus.extension.plugins.inner;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author miemie
+ * @since 2020-07-16
+ */
+class DynamicTableNameInnerInterceptorTest {
+
+    @Test
+    void doIt() {
+        // todo 逗号
+    }
+}
