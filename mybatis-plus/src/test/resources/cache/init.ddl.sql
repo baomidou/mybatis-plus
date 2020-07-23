a6ae6ab4e91b126e1212d7041f6bd8365e5ab286
nieqiurong <nieqiurong@163.com>
2020-07-23 20:18:57 +0800

    同步代码.

--- mybatis-plus/src/test/resources/cache/init.ddl.sql
+++ mybatis-plus/src/test/resources/cache/init.ddl.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS  cache (
+CREATE TABLE IF NOT EXISTS  t_cache (
 	id BIGINT(20) NOT NULL,
 	name VARCHAR(30) NULL DEFAULT NULL ,
 	PRIMARY KEY (id)
 );
-insert into cache values (1,'a');
-insert into cache values (2,'b');
-insert into cache values (3,'c');
-insert into cache values (4,'d');
-insert into cache values (5,'e');
+insert into t_cache values (1,'a');
+insert into t_cache values (2,'b');
+insert into t_cache values (3,'c');
+insert into t_cache values (4,'d');
+insert into t_cache values (5,'e');
