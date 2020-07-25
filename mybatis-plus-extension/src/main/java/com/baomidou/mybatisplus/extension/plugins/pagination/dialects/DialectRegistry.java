a51f8157e4402877163f0f3dbc3cdc6fb70db49f
miemie <wonderming@vip.qq.com>
2020-07-25 14:15:33 +0800

    精简分页方言

--- mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/plugins/pagination/dialects/DialectRegistry.java
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/plugins/pagination/dialects/DialectRegistry.java
@@ -33,25 +33,29 @@ public class DialectRegistry {
     private final Map<DbType, IDialect> dialect_enum_map = new EnumMap<>(DbType.class);
 
     public DialectRegistry() {
+        // mysql and children
         dialect_enum_map.put(DbType.MYSQL, new MySqlDialect());
-        dialect_enum_map.put(DbType.MARIADB, new MariaDBDialect());
+        dialect_enum_map.put(DbType.MARIADB, new MySqlDialect());
+        dialect_enum_map.put(DbType.GBASE, new MySqlDialect());
+        dialect_enum_map.put(DbType.OSCAR, new MySqlDialect());
+        dialect_enum_map.put(DbType.XU_GU, new MySqlDialect());
+        dialect_enum_map.put(DbType.CLICKHOUSE, new MySqlDialect());
+        // postgresql and children
+        dialect_enum_map.put(DbType.POSTGRE_SQL, new PostgreDialect());
+        dialect_enum_map.put(DbType.H2, new PostgreDialect());
+        dialect_enum_map.put(DbType.SQLITE, new PostgreDialect());
+        dialect_enum_map.put(DbType.HSQL, new PostgreDialect());
+        dialect_enum_map.put(DbType.KINGBASE_ES, new PostgreDialect());
+        dialect_enum_map.put(DbType.PHOENIX, new PostgreDialect());
+        // oracle and children
         dialect_enum_map.put(DbType.ORACLE, new OracleDialect());
+        dialect_enum_map.put(DbType.DM, new OracleDialect());
+        dialect_enum_map.put(DbType.GAUSS, new OracleDialect());
+        // other
         dialect_enum_map.put(DbType.ORACLE_12C, new Oracle12cDialect());
         dialect_enum_map.put(DbType.DB2, new DB2Dialect());
-        dialect_enum_map.put(DbType.H2, new H2Dialect());
-        dialect_enum_map.put(DbType.HSQL, new HSQLDialect());
-        dialect_enum_map.put(DbType.SQLITE, new SQLiteDialect());
-        dialect_enum_map.put(DbType.POSTGRE_SQL, new PostgreDialect());
         dialect_enum_map.put(DbType.SQL_SERVER2005, new SQLServer2005Dialect());
         dialect_enum_map.put(DbType.SQL_SERVER, new SQLServerDialect());
-        dialect_enum_map.put(DbType.DM, new DmDialect());
-        dialect_enum_map.put(DbType.XU_GU, new XuGuDialect());
-        dialect_enum_map.put(DbType.KINGBASE_ES, new KingbaseDialect());
-        dialect_enum_map.put(DbType.PHOENIX, new PhoenixDialect());
-        dialect_enum_map.put(DbType.GAUSS, new GaussDialect());
-        dialect_enum_map.put(DbType.CLICKHOUSE, new ClickhouseDialect());
-        dialect_enum_map.put(DbType.GBASE, new GBaseDialect());
-        dialect_enum_map.put(DbType.OSCAR, new OscarDialect());
     }
 
     public IDialect getDialect(DbType dbType) {
--- mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/plugins/pagination/dialects/Oracle12cDialect.java
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/plugins/pagination/dialects/Oracle12cDialect.java
@@ -27,7 +27,7 @@ public class Oracle12cDialect implements IDialect {
 
     @Override
     public DialectModel buildPaginationSql(String originalSql, long offset, long limit) {
-        String sql = originalSql + " OFFSET " + FIRST_MARK + " ROWS FETCH NEXT " + SECOND_MARK + " ROWS ONLY ";
+        String sql = originalSql + " OFFSET " + FIRST_MARK + " ROWS FETCH NEXT " + SECOND_MARK + " ROWS ONLY";
         return new DialectModel(sql, offset, limit).setConsumerChain();
     }
 }
