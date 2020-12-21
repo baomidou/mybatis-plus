c7340bd98e9f8f58de7d9589ecbec00c7f0ecc39
zhouwei <opera4130@163.com>
2020-12-21 13:45:35 +0800

    分页减少不必要的处理逻辑

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/override/MybatisMapperMethod.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/override/MybatisMapperMethod.java
@@ -81,12 +81,12 @@ public class MybatisMapperMethod {
                 } else if (method.returnsCursor()) {
                     result = executeForCursor(sqlSession, args);
                 } else {
-                    Object param = method.convertArgsToSqlCommandParam(args);
                     // TODO 这里下面改了
                     if (IPage.class.isAssignableFrom(method.getReturnType())) {
                         result = executeForIPage(sqlSession, args);
                         // TODO 这里上面改了
                     } else {
+                        Object param = method.convertArgsToSqlCommandParam(args);
                         result = sqlSession.selectOne(command.getName(), param);
                         if (method.returnsOptional()
                             && (result == null || !method.getReturnType().equals(result.getClass()))) {
