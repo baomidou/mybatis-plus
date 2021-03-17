3a9956c165a539e07e97595f32cceda61cc36ce6
miemie <wonderming@vip.qq.com>
2021-03-17 16:42:44 +0800

    test

---
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/sqlrunner/EntityMapper.java
@@ -0,0 +1,10 @@
+package com.baomidou.mybatisplus.test.sqlrunner;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author miemie
+ * @since 2021-03-16
+ */
+public interface EntityMapper extends BaseMapper<Entity> {
+}
