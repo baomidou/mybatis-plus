7d13a337d837cd8aca4d9c8877ed7fadbd484fc6
hubin <243194995@qq.com>
2021-06-16 15:03:38 +0800

    ResultMap 被修改部分恢复

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/MybatisConfiguration.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/MybatisConfiguration.java
@@ -315,8 +315,9 @@ public class MybatisConfiguration extends Configuration {
     protected void checkGloballyForDiscriminatedNestedResultMaps(ResultMap rm) {
         if (rm.hasNestedResultMaps()) {
             for (Map.Entry<String, ResultMap> entry : resultMaps.entrySet()) {
-                ResultMap entryResultMap = entry.getValue();
-                if (entryResultMap != null) {
+                Object value = entry.getValue();
+                if (value instanceof ResultMap) {
+                    ResultMap entryResultMap = (ResultMap) value;
                     if (!entryResultMap.hasNestedResultMaps() && entryResultMap.getDiscriminator() != null) {
                         Collection<String> discriminatedResultMapNames = entryResultMap.getDiscriminator().getDiscriminatorMap().values();
                         if (discriminatedResultMapNames.contains(rm.getId())) {
