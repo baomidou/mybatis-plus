ae5a3532b6120ee9da56182a77987e47ce3dfad0
sundongkai <sundongkai2012@126.com>
2021-09-28 16:31:06 +0800

    代码优化，减少重复代码

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/injector/DefaultSqlInjector.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/injector/DefaultSqlInjector.java
@@ -34,41 +34,28 @@ public class DefaultSqlInjector extends AbstractSqlInjector {
 
     @Override
     public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
+        Stream.Builder<AbstractMethod> builder = Stream.<AbstractMethod>builder()
+            .add(new Insert())
+            .add(new Delete())
+            .add(new DeleteByMap())
+            .add(new Update())
+            .add(new SelectByMap())
+            .add(new SelectCount())
+            .add(new SelectMaps())
+            .add(new SelectMapsPage())
+            .add(new SelectObjs())
+            .add(new SelectList())
+            .add(new SelectPage());
         if (tableInfo.havePK()) {
-            return Stream.of(
-                new Insert(),
-                new Delete(),
-                new DeleteByMap(),
-                new DeleteById(),
-                new DeleteBatchByIds(),
-                new Update(),
-                new UpdateById(),
-                new SelectById(),
-                new SelectBatchByIds(),
-                new SelectByMap(),
-                new SelectCount(),
-                new SelectMaps(),
-                new SelectMapsPage(),
-                new SelectObjs(),
-                new SelectList(),
-                new SelectPage()
-            ).collect(toList());
+            builder.add(new DeleteById())
+                .add(new DeleteBatchByIds())
+                .add(new UpdateById())
+                .add(new SelectById())
+                .add(new SelectBatchByIds());
         } else {
             logger.warn(String.format("%s ,Not found @TableId annotation, Cannot use Mybatis-Plus 'xxById' Method.",
                 tableInfo.getEntityType()));
-            return Stream.of(
-                new Insert(),
-                new Delete(),
-                new DeleteByMap(),
-                new Update(),
-                new SelectByMap(),
-                new SelectCount(),
-                new SelectMaps(),
-                new SelectMapsPage(),
-                new SelectObjs(),
-                new SelectList(),
-                new SelectPage()
-            ).collect(toList());
         }
+        return builder.build().collect(toList());
     }
 }
