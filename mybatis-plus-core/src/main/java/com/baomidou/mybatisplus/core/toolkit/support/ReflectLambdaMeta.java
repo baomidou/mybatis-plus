d866e9bb0a344048740dc7eef5b2a40bc1b19071
Deycoesr <Deycoesr@gmail.com>
2021-11-05 23:59:39 +0800

    Fix ReflectLambdaMeta#getCapturingClassClassLoader illegal reflective access operation

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/toolkit/support/ReflectLambdaMeta.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/toolkit/support/ReflectLambdaMeta.java
@@ -1,32 +1,15 @@
 package com.baomidou.mybatisplus.core.toolkit.support;
 
-import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
 import com.baomidou.mybatisplus.core.toolkit.ClassUtils;
-import com.baomidou.mybatisplus.core.toolkit.ReflectionKit;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.invoke.SerializedLambda;
-import java.lang.reflect.Field;
 
 /**
  * Created by hcl at 2021/5/14
  */
 @Slf4j
 public class ReflectLambdaMeta implements LambdaMeta {
-    private static final Field FIELD_CAPTURING_CLASS;
-
-    static {
-        Field fieldCapturingClass;
-        try {
-            Class<SerializedLambda> aClass = SerializedLambda.class;
-            fieldCapturingClass = ReflectionKit.setAccessible(aClass.getDeclaredField("capturingClass"));
-        } catch (Throwable e) {
-            // 解决高版本 jdk 的问题 gitee: https://gitee.com/baomidou/mybatis-plus/issues/I4A7I5
-            log.warn(e.getMessage());
-            fieldCapturingClass = null;
-        }
-        FIELD_CAPTURING_CLASS = fieldCapturingClass;
-    }
 
     private final SerializedLambda lambda;
 
@@ -47,14 +30,11 @@ public class ReflectLambdaMeta implements LambdaMeta {
     }
 
     private ClassLoader getCapturingClassClassLoader() {
-        // 如果反射失败，使用默认的 classloader
-        if (FIELD_CAPTURING_CLASS == null) {
-            return null;
-        }
         try {
-            return ((Class<?>) FIELD_CAPTURING_CLASS.get(lambda)).getClassLoader();
-        } catch (IllegalAccessException e) {
-            throw new MybatisPlusException(e);
+            return Class.forName(lambda.getCapturingClass().replace('/', '.')).getClassLoader();
+        } catch (Exception ex) {
+            log.warn(ex.getMessage());
+            return null;
         }
     }
 
