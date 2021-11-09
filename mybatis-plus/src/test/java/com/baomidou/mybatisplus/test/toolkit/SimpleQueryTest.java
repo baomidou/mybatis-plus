ebe54da9895cc56fccb29135d27a655fe4bc28aa
VampireAchao <achao1441470436@gmail.com>
2021-11-09 19:04:34 +0800

    simple-query 让简单的查询更简单

---
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/toolkit/SimpleQueryTest.java
@@ -0,0 +1,102 @@
+package com.baomidou.mybatisplus.test.toolkit;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.toolkit.SimpleQuery;
+import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
+import com.baomidou.mybatisplus.test.BaseDbTest;
+import com.baomidou.mybatisplus.test.rewrite.Entity;
+import com.baomidou.mybatisplus.test.rewrite.EntityMapper;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+/**
+ * 简单查询工具类测试
+ *
+ * @author <achao1441470436@gmail.com>
+ * @since 2021/11/9 18:30
+ */
+public class SimpleQueryTest extends BaseDbTest<EntityMapper> {
+
+    @Test
+    public void testList() {
+        // 我要这张表里的ids
+        List<Long> entityIds = SimpleQuery.list(Wrappers.lambdaQuery(), Entity::getId);
+
+        Assert.isTrue(entityIds.equals(Arrays.asList(1L, 2L)), "Ops!");
+        // 可叠加后续操作
+        List<String> names = SimpleQuery.list(Wrappers.lambdaQuery(), Entity::getName, e -> Optional.ofNullable(e.getName()).map(String::toUpperCase).ifPresent(e::setName));
+
+        Assert.isTrue(names.equals(Arrays.asList("RUBEN", "A CHAO")), "Ops!");
+    }
+
+    @Test
+    public void testMap() {
+        // 我要这个表里对应条件的用户，用id作为key给我一个map
+        Map<Long, Entity> idEntityMap = SimpleQuery.keyMap(Wrappers.<Entity>lambdaQuery().eq(Entity::getId, 1L), Entity::getId);
+        // 校验结果
+        Entity entity = new Entity();
+        entity.setId(1L);
+        entity.setName("ruben");
+        Assert.isTrue(idEntityMap.equals(Collections.singletonMap(1L, entity)), "Ops!");
+
+        // 如果我只想要id和name组成的map
+        Map<Long, String> idNameMap = SimpleQuery.map(Wrappers.lambdaQuery(), Entity::getId, Entity::getName);
+        // 校验结果
+        Map<Long, String> map = new HashMap<>(1 << 2);
+        map.put(1L, "ruben");
+        map.put(2L, "a chao");
+        Assert.isTrue(idNameMap.equals(map), "Ops!");
+
+        // 同样支持叠加后续操作
+//        SimpleQuery.keyMap(Wrappers.lambdaQuery(), Entity::getId, System.out::println, System.out::println);
+
+    }
+
+    @Test
+    public void testGroup() {
+        // 我需要相同名字的用户的分为一组，再造一条数据
+        BaseMapper<Entity> mapper = SqlHelper.getMapper(Entity.class);
+        Entity entity = new Entity();
+        entity.setId(3L);
+        entity.setName("ruben");
+        mapper.insert(entity);
+
+        // 简单查询
+        Map<String, List<Entity>> nameUsersMap = SimpleQuery.group(Wrappers.lambdaQuery(), Entity::getName);
+
+        // 校验结果
+        Map<String, List<Entity>> map = new HashMap<>(1 << 2);
+        Entity chao = new Entity();
+        chao.setId(2L);
+        chao.setName("a chao");
+        map.put("a chao", Collections.singletonList(chao));
+
+        Entity ruben = new Entity();
+        ruben.setId(1L);
+        ruben.setName("ruben");
+        Entity ruben2 = new Entity();
+        ruben2.setId(3L);
+        ruben2.setName("ruben");
+        map.put("ruben", Arrays.asList(ruben, ruben2));
+        Assert.isTrue(nameUsersMap.equals(map), "Ops!");
+
+    }
+
+    @Override
+    protected String tableDataSql() {
+        return "insert into entity(id,name) values(1,'ruben'),(2,'a chao');";
+    }
+
+    @Override
+    protected List<String> tableSql() {
+        return Arrays.asList("drop table if exists entity", "CREATE TABLE IF NOT EXISTS entity (" +
+            "id BIGINT NOT NULL," +
+            "name VARCHAR(30) NULL DEFAULT NULL," +
+            "PRIMARY KEY (id))");
+    }
+
+}
+
