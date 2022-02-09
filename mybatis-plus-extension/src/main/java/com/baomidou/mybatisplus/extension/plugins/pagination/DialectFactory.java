580d91374228121e48a547d25c15c23b70fcef37
miemie <wonderming@vip.qq.com>
2022-02-09 15:16:45 +0800

    code clean

--- mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/plugins/pagination/DialectFactory.java
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/plugins/pagination/DialectFactory.java
@@ -35,7 +35,7 @@ public class DialectFactory {
         IDialect dialect = DIALECT_ENUM_MAP.get(dbType);
         if (null == dialect) {
             if (dbType == DbType.OTHER) {
-                ExceptionUtils.mpe("%s database not supported.", dbType.getDb());
+                throw ExceptionUtils.mpe("%s database not supported.", dbType.getDb());
             }
             // mysql same type
             else if (dbType == DbType.MYSQL
@@ -64,10 +64,10 @@ public class DialectFactory {
                 || dbType == DbType.KINGBASE_ES
                 || dbType == DbType.PHOENIX
                 || dbType == DbType.SAP_HANA
-                || dbType == DbType.IMPALA) {
+                || dbType == DbType.IMPALA
+                || dbType == DbType.HIGH_GO
+                || dbType == DbType.VERTICA) {
                 dialect = new PostgreDialect();
-            } else if (dbType == DbType.HIGH_GO) {
-                dialect = new HighGoDialect();
             } else if (dbType == DbType.ORACLE_12C) {
                 dialect = new Oracle12cDialect();
             } else if (dbType == DbType.DB2) {
@@ -78,10 +78,8 @@ public class DialectFactory {
                 dialect = new SQLServerDialect();
             } else if (dbType == DbType.SYBASE) {
                 dialect = new SybaseDialect();
-            } else if (dbType == DbType.GBASEDBT){
+            } else if (dbType == DbType.GBASEDBT) {
                 dialect = new GBasedbtDialect();
-            } else if (dbType == DbType.VERTICA) {
-                dialect = new VerticaDialect();
             }
             DIALECT_ENUM_MAP.put(dbType, dialect);
         }
