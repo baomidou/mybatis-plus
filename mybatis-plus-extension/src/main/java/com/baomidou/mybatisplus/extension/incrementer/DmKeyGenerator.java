f3d668e76f35c5dad6b4ddf17a438fa828939ddf
miemie <wonderming@vip.qq.com>
2022-05-31 00:05:56 +0800

    license

--- mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/incrementer/DmKeyGenerator.java
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/incrementer/DmKeyGenerator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2011-2022, baomidou (jobob@qq.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.baomidou.mybatisplus.extension.incrementer;
 
 import com.baomidou.mybatisplus.annotation.DbType;
@@ -11,13 +26,13 @@ import com.baomidou.mybatisplus.core.incrementer.IKeyGenerator;
  */
 public class DmKeyGenerator implements IKeyGenerator {
 
-	@Override
-	public String executeSql(String incrementerName) {
-		return "SELECT " + incrementerName + ".NEXTVAL FROM DUAL";
-	}
+    @Override
+    public String executeSql(String incrementerName) {
+        return "SELECT " + incrementerName + ".NEXTVAL FROM DUAL";
+    }
 
-	@Override
-	public DbType dbType() {
-		return DbType.DM;
-	}
+    @Override
+    public DbType dbType() {
+        return DbType.DM;
+    }
 }
--- mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/toolkit/SimpleQuery.java
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/toolkit/SimpleQuery.java
@@ -1,26 +1,31 @@
+/*
+ * Copyright (c) 2011-2022, baomidou (jobob@qq.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.baomidou.mybatisplus.extension.toolkit;
-
-import java.util.Collections;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * simple-query 让简单的查询更简单
