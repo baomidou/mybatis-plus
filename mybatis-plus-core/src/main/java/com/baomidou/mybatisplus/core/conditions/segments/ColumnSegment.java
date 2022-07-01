af7042fa37f57e10b32f398313e41db87e7d9548
mcck <1445499792@qq.com>
2022-07-01 02:46:41 +0000

    添加Column类型接口

---
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/conditions/segments/ColumnSegment.java
@@ -0,0 +1,7 @@
+package com.baomidou.mybatisplus.core.conditions.segments;
+
+import com.baomidou.mybatisplus.core.conditions.ISqlSegment;
+
+@FunctionalInterface
+public interface ColumnSegment extends ISqlSegment {
+}
