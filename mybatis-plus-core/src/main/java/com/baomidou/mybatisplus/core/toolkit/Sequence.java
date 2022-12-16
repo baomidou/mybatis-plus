740741abb1c98bf72096bf84526e6de5313b5ca2
huangyebiaoke <huangyebiaoke@outlook.com>
2022-12-16 17:01:15 +0800

    优化注释

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/toolkit/Sequence.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/toolkit/Sequence.java
@@ -174,7 +174,7 @@ public class Sequence {
                 timestamp = tilNextMillis(lastTimestamp);
             }
         } else {
-            // 不同毫秒内，序列号置为 1 - 3 随机数
+            // 不同毫秒内，序列号置为 1 - 2 随机数
             sequence = ThreadLocalRandom.current().nextLong(1, 3);
         }
 
