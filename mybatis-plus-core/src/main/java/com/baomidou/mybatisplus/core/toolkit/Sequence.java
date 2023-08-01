0d2718e8656e30be23728e32bbb1519eb653318d
nieqiurong <nieqiurong@163.com>
2023-08-01 19:52:40 +0800

    增加Sequence初始化日志.
    https://github.com/baomidou/mybatis-plus/issues/5233

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/toolkit/Sequence.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/toolkit/Sequence.java
@@ -80,6 +80,13 @@ public class Sequence {
         this.inetAddress = inetAddress;
         this.datacenterId = getDatacenterId(maxDatacenterId);
         this.workerId = getMaxWorkerId(datacenterId, maxWorkerId);
+        initLog();
+    }
+
+    private void initLog() {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Initialization Sequence datacenterId:" + this.datacenterId + " workerId:" + this.workerId);
+        }
     }
 
     /**
@@ -95,6 +102,7 @@ public class Sequence {
             String.format("datacenter Id can't be greater than %d or less than 0", maxDatacenterId));
         this.workerId = workerId;
         this.datacenterId = datacenterId;
+        initLog();
     }
 
     /**
