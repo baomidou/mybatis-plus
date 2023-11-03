7840566894ca0a5762b8517c8a519f162de5b79d
nieqiurong <nieqiurong@163.com>
2023-11-04 00:11:45 +0800

    发布3.5.4.1

---
+++ mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/h2/issues/repositoryscan/service/IDemoRepositoryService.java
@@ -0,0 +1,11 @@
+package com.baomidou.mybatisplus.test.h2.issues.repositoryscan.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.test.h2.issues.repositoryscan.entity.Demo;
+
+/**
+ * @author nieqiurong
+ */
+public interface IDemoRepositoryService extends IService<Demo> {
+
+}
