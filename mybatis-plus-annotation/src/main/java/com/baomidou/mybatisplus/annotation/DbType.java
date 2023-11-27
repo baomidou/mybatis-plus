27ffd17bc3635b1d946024f3ff8c01c3defadefa
gailibing <gailibing@163.com>
2023-11-27 09:31:51 +0800

    添加对GBase8s-pg的支持

--- mybatis-plus-annotation/src/main/java/com/baomidou/mybatisplus/annotation/DbType.java
+++ mybatis-plus-annotation/src/main/java/com/baomidou/mybatisplus/annotation/DbType.java
@@ -105,14 +105,14 @@ public enum DbType {
      */
     GBASE_8S("gbase-8s", "南大通用数据库 GBase 8s"),
     /**
-     * GBase8sPG
-     */
-    GBASE8S_PG("gbase8s-pg", "南大通用数据库 GBase 8s兼容pg"),
-    /**
      * use {@link  #GBASE_8S}
      *
      * @deprecated 2022-05-30
      */
+    /**
+     * GBase8sPG
+     */
+    GBASE8S_PG("gbase8s-pg", "南大通用数据库 GBase 8s兼容pg"),
     @Deprecated
     GBASEDBT("gbasedbt", "南大通用数据库"),
     /**
