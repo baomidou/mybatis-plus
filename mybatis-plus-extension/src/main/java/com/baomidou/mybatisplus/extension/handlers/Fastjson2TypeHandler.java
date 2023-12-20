e9ff15e59970e45d75da64afe862def4f030e962
nieqiurong <nieqiurong@163.com>
2023-12-20 12:50:55 +0800

    增加Fastjson2支持.

---
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/handlers/Fastjson2TypeHandler.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2011-2023, baomidou (jobob@qq.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.baomidou.mybatisplus.extension.handlers;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONWriter;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+/**
+ * Fastjson2 实现 JSON 字段类型处理器
+ *
+ * @author nieqiurong
+ * @since 3.5.5
+ */
+@Slf4j
+@MappedTypes({Object.class})
+@MappedJdbcTypes(JdbcType.VARCHAR)
+public class Fastjson2TypeHandler extends AbstractJsonTypeHandler<Object> {
+
+    private final Class<?> type;
+
+    public Fastjson2TypeHandler(Class<?> type) {
+        if (log.isTraceEnabled()) {
+            log.trace("Fastjson2TypeHandler(" + type + ")");
+        }
+        Assert.notNull(type, "Type argument cannot be null");
+        this.type = type;
+    }
+
+
+    @Override
+    protected Object parse(String json) {
+        return JSON.parseObject(json, this.type);
+    }
+
+    @Override
+    protected String toJson(Object obj) {
+        return JSON.toJSONString(obj, JSONWriter.Feature.WriteMapNullValue,
+            JSONWriter.Feature.WriteNullListAsEmpty, JSONWriter.Feature.WriteNullStringAsEmpty);
+    }
+
+}
