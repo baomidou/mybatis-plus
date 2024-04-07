6ce18f5a237ddcd619379263717eb05daaf403dd
VampireAchao <achao@apache.org>
2024-04-07 15:31:08 +0800

    optimize import

--- mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/handlers/JacksonTypeHandler.java
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/handlers/JacksonTypeHandler.java
@@ -17,7 +17,6 @@ package com.baomidou.mybatisplus.extension.handlers;
 
 import com.baomidou.mybatisplus.core.toolkit.Assert;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedJdbcTypes;
@@ -25,7 +24,6 @@ import org.apache.ibatis.type.MappedTypes;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.lang.reflect.Type;
 
 /**
  * Jackson 实现 JSON 字段类型处理器
