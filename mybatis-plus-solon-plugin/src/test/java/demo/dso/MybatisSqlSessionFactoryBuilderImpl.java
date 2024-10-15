a757e5f9660dcca62bd97a9e331864e28f3d3c66
hubin <jobob@qq.com>
2024-10-15 18:40:43 +0800

    移除 solon-plugin 模块独立仓库开发

--- mybatis-plus-solon-plugin/src/test/java/demo/dso/MybatisSqlSessionFactoryBuilderImpl.java
+++
@@ -1,9 +0,0 @@
-package demo.dso;
-
-import com.baomidou.mybatisplus.core.MybatisSqlSessionFactoryBuilder;
-
-/**
- * @author noear 2021/10/20 created
- */
-public class MybatisSqlSessionFactoryBuilderImpl extends MybatisSqlSessionFactoryBuilder {
-}
