82237d82a1fc0d10f38f292771a2356ac1b817d0
nieqiurong <nieqiurong@163.com>
2025-08-29 11:35:30 +0800

    依赖调整.

--- spring-boot-starter/mybatis-plus-spring-boot3-starter/src/test/java/com/baomidou/mybatisplus/test/pom/GeneratePomTest.java
+++ spring-boot-starter/mybatis-plus-spring-boot3-starter/src/test/java/com/baomidou/mybatisplus/test/pom/GeneratePomTest.java
@@ -62,7 +62,7 @@ class GeneratePomTest {
             Assertions.assertFalse(bom.isOptional());
             Assertions.assertEquals("4.1.4", dependenciesMap.get("spring-cloud-commons").getVersion());
             Assertions.assertEquals("3.0.5", dependenciesMap.get("mybatis-spring").getVersion());
-            Assertions.assertEquals("3.4.1", dependenciesMap.get("spring-boot-dependencies").getVersion());
+            Assertions.assertEquals("3.5.0", dependenciesMap.get("spring-boot-dependencies").getVersion());
         }
     }
 
