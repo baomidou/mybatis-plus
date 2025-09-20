c28bfc1b4f85c6dbe34d3d80ba71295d4108a8b0
xiaomeng.milo@gmail.com <2240241948@qq.com>
2025-09-20 21:58:47 +0800

    feat(mybatis-plus-extension): 添加对 Jackson 3 的支持

---
+++ mybatis-plus-extension/src/main/java/com/baomidou/mybatisplus/extension/handlers/Jackson3TypeHandler.java
@@ -0,0 +1,55 @@
+package com.baomidou.mybatisplus.extension.handlers;
+
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import tools.jackson.databind.JavaType;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.type.TypeFactory;
+
+
+import java.lang.reflect.Field;
+import java.util.Objects;
+
+/**
+ * jackson tools 实现的字段类型处理器
+ *
+ * @author milo
+ * @since 2025-09-20 21:47
+ */
+public class Jackson3TypeHandler extends AbstractJsonTypeHandler<Object> {
+    private static ObjectMapper OBJECT_MAPPER;
+
+    public Jackson3TypeHandler(Class<?> type) {
+        super(type);
+    }
+
+    public Jackson3TypeHandler(Class<?> type, Field field) {
+        super(type, field);
+    }
+
+    @Override
+    public Object parse(String json) {
+        ObjectMapper objectMapper = getObjectMapper();
+        TypeFactory typeFactory = objectMapper.getTypeFactory();
+        JavaType javaType = typeFactory.constructType(getFieldType());
+        return objectMapper.readValue(json, javaType);
+    }
+
+    @Override
+    public String toJson(Object obj) {
+        return getObjectMapper().writeValueAsString(obj);
+    }
+
+    public static ObjectMapper getObjectMapper() {
+        if (Objects.nonNull(OBJECT_MAPPER)) return OBJECT_MAPPER;
+        synchronized (Jackson3TypeHandler.class){
+            if (Objects.nonNull(OBJECT_MAPPER)) return OBJECT_MAPPER;
+            OBJECT_MAPPER = new ObjectMapper();
+        }
+        return OBJECT_MAPPER;
+    }
+
+    public static void setObjectMapper(ObjectMapper objectMapper) {
+        Assert.notNull(objectMapper, "ObjectMapper should not be null");
+        Jackson3TypeHandler.OBJECT_MAPPER = objectMapper;
+    }
+}
